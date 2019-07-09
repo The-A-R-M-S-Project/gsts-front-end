f9927ca30682c1f279595ee20389d637acfe33b1
Ben-Cliff <benclifg@gmail.com>
2019-07-09 12:46:50 +0300

    [Rodrick] adds login component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,30 +2,39 @@
   <v-container fluid>
     <v-card>
       <v-layout row wrap>
-        <v-flex sm8>
+        <v-flex sm7 pa-3>
           <v-card-text>
             <!-- heading -->
-            <h1 class="text-sm-center">Login</h1>
+            <h1 class="text-xs-center">Login</h1>
             <!-- login form -->
             <v-container>
-                  <v-tabs
-                    v-model="tabs"
-                    fixed-tabs
-                    flat
+                <v-tabs
+                  v-model="tabs"
+                  fixed-tabs
+                  flat
+                >
+                  <v-tabs-slider></v-tabs-slider>
+                  <v-tab href="#mobile-tabs-5-1" class="primary--text">
+                    <p>Admin</p>
+                  </v-tab>
+        
+                  <v-tab href="#mobile-tabs-5-2" class="primary--text">
+                    <p>Student</p>
+                  </v-tab>
+                </v-tabs>
+                <v-tabs-items v-model="tabs" class="white elevation-0">
+                  <v-tab-item
+                    v-for="i in 2"
+                    :key="i"
+                    :value="'mobile-tabs-5-' + i"
                   >
-                    <v-tabs-slider></v-tabs-slider>
-                    <v-tab href="#mobile-tabs-5-1" class="primary--text">
-                      <p>Admin</p>
-                    </v-tab>
-          
-                    <v-tab href="#mobile-tabs-5-2" class="primary--text">
-                      <p>Student</p>
-                    </v-tab>
-                  </v-tabs>
+                      <LoginForm/>
+                  </v-tab-item>
+                </v-tabs-items>
             </v-container>
           </v-card-text>
         </v-flex>
-        <v-flex sm4>
+        <v-flex sm5>
           <v-card-text>Sign up info goes here</v-card-text>
         </v-flex>        
       </v-layout>
@@ -34,10 +43,10 @@
 </template>
 
 <script>
-  
+  import LoginForm from "@/components/LoginForm"
   export default {
     components: {
-      
+      LoginForm,
     }
   }
 </script>
