1114569fcdf0fd448801334f6a5e3f2d876f1fc5
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-07-09 09:37:52 +0300

    [Rodrick/wycliff] Adds vuetify grid to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,16 @@
 <template>
-<div>This is the default page. Probably to be converted to Dashboard.vue</div>
+  <v-container fluid>
+    <v-card>
+      <v-layout row wrap>
+        <v-flex sm7>
+          <v-card-text>Login forms go here</v-card-text>
+        </v-flex>
+        <v-flex sm5>
+          <v-card-text>Sign up info goes here</v-card-text>
+        </v-flex>        
+      </v-layout>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
