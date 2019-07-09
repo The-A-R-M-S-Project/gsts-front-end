c9a069a024ff6b1bd7b6b47adccab61cfe07f792
Ben-Cliff <benclifg@gmail.com>
2019-07-09 12:42:45 +0300

    [Rodrick] adds login form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-form>
+      <v-container>
+           <v-text-field
+                label="email"
+                prepend-inner-icon="person"
+                type="email"
+           >
+           </v-text-field>
+            <v-text-field
+                label="password"
+                prepend-inner-icon="lock"
+                type="password"
+            >
+            </v-text-field>
+            <div class="text-xs-center">
+                <v-btn round  class="text-sm-center">Login</v-btn>
+            </div>
+      </v-container>
+    </v-form>
+</template>
+<script>
+export default{
+    name: "LoginForm",
+}
+</script>
