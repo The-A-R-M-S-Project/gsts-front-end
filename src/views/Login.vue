b8cb102e1e37ac200a82b1e52c0444cf1f12975c
Ben-Cliff <benclifg@gmail.com>
2019-07-09 10:34:20 +0300

    [Rodrick] adds tabs on login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,13 +2,30 @@
   <v-container fluid>
     <v-card>
       <v-layout row wrap>
-        <v-flex sm7>
+        <v-flex sm8>
           <v-card-text>
+            <!-- heading -->
             <h1 class="text-sm-center">Login</h1>
-            Login forms go here
+            <!-- login form -->
+            <v-container>
+                  <v-tabs
+                    v-model="tabs"
+                    fixed-tabs
+                    flat
+                  >
+                    <v-tabs-slider></v-tabs-slider>
+                    <v-tab href="#mobile-tabs-5-1" class="primary--text">
+                      <p>Admin</p>
+                    </v-tab>
+          
+                    <v-tab href="#mobile-tabs-5-2" class="primary--text">
+                      <p>Student</p>
+                    </v-tab>
+                  </v-tabs>
+            </v-container>
           </v-card-text>
         </v-flex>
-        <v-flex sm5>
+        <v-flex sm4>
           <v-card-text>Sign up info goes here</v-card-text>
         </v-flex>        
       </v-layout>
