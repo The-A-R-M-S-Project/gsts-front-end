f1311d2de5eef3b5b93856af991bc30dc2812646
Ben-Cliff <benclifg@gmail.com>
2019-07-10 12:43:12 +0300

    [Rodrick] adds register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -11,23 +11,94 @@
                       <p>To keep track of your report please
                           login with your personal information
                       </p>
-                      <v-btn dark round>Sign in</v-btn>
+                      <v-btn dark round to="/">login</v-btn>
                   </v-flex>
                 </v-layout>   
               </v-container>
             </v-flex>
-            <v-flex sm7 pa-3>
-              <v-card-text>
+            <v-flex sm7>
                 <v-container>
                     <h1>Sign Up</h1>
+                    <v-form v-model="valid">
+                        <v-container class="container">
+                        <v-layout>
+                            <v-flex>
+                                <v-text-field
+                                    v-model="firstname"
+                                    :rules="nameRules"
+                                    :counter="10"
+                                    label="First name"
+                                    required
+                                    prepend-inner-icon="person"
+                                    height="28"
+                                ></v-text-field>
+                            </v-flex>
+
+                            <v-flex>
+                                <v-text-field
+                                    v-model="lastname"
+                                    :rules="nameRules"
+                                    :counter="10"
+                                    label="Last name"
+                                    required
+                                    prepend-inner-icon="person"
+                                    height="28"
+                                ></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                        <v-text-field
+                            v-model="firstname"
+                            label="Registration Number"
+                            required
+                            prepend-inner-icon="person"
+                            height="28"
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="email"
+                            label="College Email"
+                            required
+                            prepend-inner-icon="person"
+                            height="28"
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="email"
+                            label="Username/Email"
+                            required
+                            prepend-inner-icon="person"
+                            height="28"
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="password"
+                            label="Password"
+                            required
+                            type="password"
+                            prepend-inner-icon="lock"
+                            height="28"
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="password"
+                            label="Confirm Password"
+                            required
+                            type="password"
+                            prepend-inner-icon="lock"
+                            height="28"
+                        ></v-text-field>
+                        </v-container>
+                        <div class="text-xs-center">
+                            <v-btn light>sign up</v-btn>
+                        </div>
+                    </v-form>
                 </v-container>
-              </v-card-text>
             </v-flex>     
           </v-layout>
         </v-card>
     </v-layout>
   </v-container>
 </template>
+<style lang="scss">
+.container{
+}
+</style>
 
 <script>
   export default {
