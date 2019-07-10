6c9335b528dcf34254fb64c6839d80168a33c64c
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-10 11:30:07 +0300

    [Wycliff] adds presubmission validation to the login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,13 +1,18 @@
 <template>
-    <v-form>
+    <v-form ref="form" v-model="form">
       <v-container>
            <v-text-field
+                v-model="email"
+                :rules="[rules.email]"
                 label="College Email"
                 prepend-inner-icon="person"
                 type="email"
            >
            </v-text-field>
             <v-text-field
+                v-model="password"
+                :rules="[rules.password, rules.length(8)]"
+                counter="8"
                 label="password"
                 prepend-inner-icon="lock"
                 type="password"
@@ -22,5 +27,17 @@
 <script>
 export default{
     name: "LoginForm",
+    data: () => ({
+      email: undefined,
+      form: false,
+      password: undefined,
+      rules: {
+        email: v => (v || '').match(/@cedat.mak.ac.ug/) || 'Please enter a valid college email',
+        length: len => v => (v || '').length >= len || `Invalid character length, required ${len}`,
+        password: v => (v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
+          'Must contain an uppercase letter, a numeric & special characters',
+        required: v => !!v || 'This field is required'
+      }
+    })
 }
 </script>
