4d5cd540c0772f1260b499db3c03861eba4a9d03
Ben-Cliff <benclifg@gmail.com>
2019-07-10 09:19:52 +0300

    Vertically center aligns login card

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,48 +1,54 @@
 <template>
-  <v-container fluid>
-    <v-card>
-      <v-layout row wrap>
-        <v-flex sm7 pa-3>
-          <v-card-text>
-            <!-- heading -->
-            <h1 class="text-xs-center">Login</h1>
-            <!-- login form -->
-            <v-container>
-                <v-tabs
-                  v-model="tabs"
-                  fixed-tabs
-                  flat
-                >
-                  <v-tabs-slider></v-tabs-slider>
-                  <v-tab href="#mobile-tabs-5-1" class="primary--text">
-                    <p>Admin</p>
-                  </v-tab>
-        
-                  <v-tab href="#mobile-tabs-5-2" class="primary--text">
-                    <p>Student</p>
-                  </v-tab>
-                </v-tabs>
-                <v-tabs-items v-model="tabs" class="white elevation-0">
-                  <v-tab-item
-                    v-for="i in 2"
-                    :key="i"
-                    :value="'mobile-tabs-5-' + i"
-                  >
-                      <LoginForm/>
-                  </v-tab-item>
-                </v-tabs-items>
-            </v-container>
-          </v-card-text>
-        </v-flex>
-        <v-flex sm5>
-          <div class="overlayed text-xs-center">
-            <h1>Graduate Student Tracking System</h1>
-            <h3>Are you a student? Register Here!</h3>
-            <v-btn round dark>Sign up</v-btn>
-          </div>
-        </v-flex>        
-      </v-layout>
-    </v-card>
+  <v-container text-xs-center fluid fill-height>
+    <v-layout align-center justify-center>
+        <v-card>
+          <v-layout row wrap>
+            <v-flex sm7 pa-3>
+              <v-card-text>
+                <!-- heading -->
+                <h1 class="text-xs-center">Login</h1>
+                <!-- login form -->
+                <v-container>
+                    <v-tabs
+                      v-model="tabs"
+                      fixed-tabs
+                      flat
+                    >
+                      <v-tabs-slider></v-tabs-slider>
+                      <v-tab href="#mobile-tabs-5-1" class="primary--text">
+                        <p>Admin</p>
+                      </v-tab>
+            
+                      <v-tab href="#mobile-tabs-5-2" class="primary--text">
+                        <p>Student</p>
+                      </v-tab>
+                    </v-tabs>
+                    <v-tabs-items v-model="tabs" class="white elevation-0">
+                      <v-tab-item
+                        v-for="i in 2"
+                        :key="i"
+                        :value="'mobile-tabs-5-' + i"
+                      >
+                          <LoginForm/>
+                      </v-tab-item>
+                    </v-tabs-items>
+                </v-container>
+              </v-card-text>
+            </v-flex>
+            <v-flex sm5>
+              <v-container fill-height pa-0 overlayed>
+                <v-layout align-center>
+                  <v-flex>
+                      <h1>Graduate Student Tracking System</h1>
+                      <p>Are you a student? Register Here!</p>
+                      <v-btn round dark>Register</v-btn>
+                  </v-flex>
+                </v-layout>   
+              </v-container>
+            </v-flex>     
+          </v-layout>
+        </v-card>
+    </v-layout>
   </v-container>
 </template>
 
@@ -51,6 +57,11 @@
   export default {
     components: {
       LoginForm,
+    },
+    data(){
+      return{
+        tabs: null,
+      }
     }
   }
 </script>
@@ -63,6 +74,14 @@
   position: relative;
   font-family: 'Comfortaa', cursive;
   color: white;
+}
+.overlayed p {
+  font-size: 18px;
+  letter-spacing: 150%;
+}
+.overlayed h1{
+  font-size: 25px;
+  font-weight: lighter;
 }
 </style>
 
