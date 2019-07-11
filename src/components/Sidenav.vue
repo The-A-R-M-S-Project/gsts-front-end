5a9d200259f0b912d8cd11fd8424fd496105b10d
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-11 18:44:37 +0300

    [Wycliff] adds side navigation components for dashboard view.

--- src/components/Sidenav.vue
+++ src/components/Sidenav.vue
@@ -1,28 +1,38 @@
 <template>
-  <div>
-    <v-navigation-drawer permanent>
-      <v-toolbar flat>
+    <v-navigation-drawer 
+        permanent
+        absolute
+    >
+        <v-toolbar flat>
         <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">
-              <v-icon>account_circle</v-icon>
-              Admin
-            </v-list-tile-title>
-          </v-list-tile>
+            <v-list-tile avatar>
+            <v-list-tile-avatar>
+                <img src="~@/assets/img/joyce-mccown-whitebooks.jpg">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+                <v-list-tile-title class="title">Admin</v-list-tile-title>
+            </v-list-tile-content>
+            </v-list-tile>
         </v-list>
-      </v-toolbar>
-      <v-divider></v-divider>
-      <v-list dense>
+        </v-toolbar>
+        <v-divider></v-divider>
+        <v-list dense>
         <v-list-tile
-          v-for="item in items"
-          :key="item.title">
-          <v-list-tile-content>
+            v-for="item in items"
+            :key="item.title"
+            v-on:click=""
+            >
+            <v-list-tile-content>
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
+            </v-list-tile-content>
         </v-list-tile>
-      </v-list>
+        <v-list-tile fixed>
+            <v-list-tile-content>
+                <v-list-tile-title>Logout</v-list-tile-title>
+            </v-list-tile-content>
+        </v-list-tile>
+        </v-list>
     </v-navigation-drawer>
-  </div>
 </template>
 <script>
 export default {
@@ -30,12 +40,9 @@ export default {
     data () {
       return {
         items: [
-          { title: 'Dashboard'},
           { title: 'Notifications'},
           { title: 'Calendar'},
-          { title: 'Logout'},
         ],
-        // right: null
       }
     }
 }
