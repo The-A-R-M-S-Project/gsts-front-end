56e3f10f44191f8c37ced7253b5d7a46d2f5e947
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-11 11:28:54 +0300

    [Wycliff] creates navbar and routes the dashboard

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-toolbar
+      flat
+      dark
+      color="grey darken-3"
+    >
+      <v-toolbar-title>G.S.T.S</v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+<script>
+export default {
+    name: 'navbar',
+}
+</script>
+
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
+import Dashboard from "./views/Dashboard.vue";
 
 
 Vue.use(Router);
@@ -17,6 +18,11 @@ export default new Router({
       path: "/register",
       name: "register",
       component: Register
-    }
+    },
+    {
+      path: "/dashboard",
+      name: "dasboard",
+      component: Dashboard
+    },
   ]
 });
