0fc34e0376ac236d8ea189d37f4ef40d4c667c6f
Ben-Cliff <benclifg@gmail.com>
2019-07-11 21:54:41 +0300

    Creates div with padding

---
+++ src/views/AdminDashboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <Navigation/>
+      <div class="pa-3">
+        <v-card flat min-height="95vh">
+          <Sidenav/>
+        </v-card>
+      </div>
+    </div>
+</template>
+
+<script>
+import Navigation from "@/components/Navbar.vue"
+import Sidenav from "@/components/Sidenav.vue"
+export default {
+    name: 'dashboard',
+    components: {
+        Navigation,
+        Sidenav,
+    },
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
