c298231bca3c8c395d6bccb24c4d42f209862127
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-11 11:47:27 +0300

    [Wycliff] adds side navigation to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,22 +1,19 @@
 <template>
     <div>
         <Navigation/>
-        
+        <Sidenav/>
     </div>
 </template>
 
 <script>
 import Navigation from "@/components/Navbar.vue"
-
+import Sidenav from "@/components/Sidenav.vue"
 export default {
     name: 'dashboard',
     components: {
         Navigation,
-        
+        Sidenav,
     },
-    data: {
-
-    }
 }
 </script>
 
