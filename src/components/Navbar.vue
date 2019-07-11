2cd0ec14196222f3a2b91be36a81f01e59d1e3dd
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-11 18:40:39 +0300

    [Wycliff] adds buttons to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <v-toolbar
-      flat
-      dark
-      color="grey darken-3"
-    >
-      <v-toolbar-title>G.S.T.S</v-toolbar-title>
+    <v-toolbar flat dark color="grey darken-3">
+      <v-toolbar-title>
+        G.S.T.S
+      </v-toolbar-title>
+      <div>
+        <v-btn flat small>Dashboard</v-btn>
+        <v-btn flat small>Departments</v-btn>
+        <v-btn flat small>Examiners</v-btn>  
+      </div>
     </v-toolbar>
   </div>
 </template>
