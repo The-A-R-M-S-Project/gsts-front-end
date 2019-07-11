42124c32cbe9327a0fbe0ff1c7d8b557511de6f7
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-11 11:28:54 +0300

    [Wycliff] creates navbar and routes the dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,22 @@
 <template>
-    
+    <div>
+        <Navigation/>
+        
+    </div>
 </template>
 
 <script>
+import Navigation from "@/components/Navbar.vue"
+
 export default {
-    
+    name: 'dashboard',
+    components: {
+        Navigation,
+        
+    },
+    data: {
+
+    }
 }
 </script>
 
