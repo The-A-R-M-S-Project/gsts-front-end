d924f58c3b19fa7eccae5d2ec68628f805be84c2
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-12 12:35:58 +0300

    [Wycliff] adds VivaStatus component

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -4,20 +4,20 @@
       <div class="pa-3">
         <v-card flat min-height="95vh" class="transparent">
           <v-layout row wrap>
-            <v-flex>
+            <v-flex sm2>
               <Sidenav/>
             </v-flex>
             <v-flex sm10>
               <v-card class="transparent" flat>
                 <v-layout row wrap>
-                  <v-flex sm6 pb-3 pr-2> 
-                    <v-card>
-                      <v-card-text>
-                        viva status
+                  <v-flex sm5 pb-3 pr-2> 
+                    <v-card min-height="40vh">
+                      <v-card-text align-center>
+                        <VivaStatus/>
                       </v-card-text>
                     </v-card>
                   </v-flex>
-                  <v-flex sm6 pb-3 pl-2 min-height="">   
+                  <v-flex sm7 pb-3 pl-2 min-height="">   
                     <v-card>
                       <v-card-text>
                         report status
@@ -50,11 +50,13 @@
 <script>
 import Navigation from "@/components/Navbar.vue"
 import Sidenav from "@/components/Sidenav.vue"
+import VivaStatus from "@/components/VivaStatus.vue"
 export default {
     name: 'dashboard',
     components: {
         Navigation,
         Sidenav,
+        VivaStatus
     },
 }
 </script>
