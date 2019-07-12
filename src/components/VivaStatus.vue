bfe9a7ba808562561a347f12043bc3042791d44b
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-12 12:35:00 +0300

    [Wycliff] creates viva status chart component

---
+++ src/components/VivaStatus.vue
@@ -0,0 +1,56 @@
+<template>
+    <canvas id="donutChart"></canvas>    
+</template>
+<script>
+import Chart from 'chart.js'
+export default {
+    name: 'VivaStatus',
+    data() {
+        return{
+            chartData: {
+                type: 'doughnut',
+                data: {
+                    datasets: [{
+                        label: 'Viva Status',
+                        data: [10, 20],
+                        backgroundColor: [
+                            'rgba(165,105,189,1',
+                            'rgba(232,218,239,1)'
+                        ],
+                    }],
+                    labels: [
+                        'done',
+                        'pending'
+                    ],
+                },    
+                options: {
+                    title: {
+                        display: true,
+                        text: 'Viva Status',
+                        fontSize: 20,
+                        defaultFontFamily: "'Comfortaa', cursive"
+                    },
+                    responsive: true,
+                    legend: {
+                        position: 'bottom',
+                    },
+                    cutoutPercentage: 80
+
+                }
+            },
+        }
+    },
+    methods: {
+        createChart(chartId, chartData) {
+            const ctx = document.getElementById(chartId);
+            const myChart = new Chart(ctx, {
+                type: chartData.type,
+                data: chartData.data,
+                options: chartData.options,
+        });
+    }},
+    mounted() {
+        this.createChart('donutChart', this.chartData)
+    }
+}
+</script>
