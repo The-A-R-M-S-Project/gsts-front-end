31a6b0d201b2911c7e627267cf6b9c3402011d18
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-12 12:35:58 +0300

    [Wycliff] adds VivaStatus component

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -1,54 +1,65 @@
 <template>
-  <div>
-    <Navigation />
-    <div class="pa-3">
-      <v-card flat min-height="95vh" class="transparent">
-        <v-layout row wrap>
-          <v-flex>
-            <Sidenav />
-          </v-flex>
-          <v-flex sm10>
-            <v-card class="transparent" flat>
-              <v-layout row wrap>
-                <v-flex sm6 pb-3 pr-2>
-                  <v-card>
-                    <v-card-text>viva status</v-card-text>
-                  </v-card>
-                </v-flex>
-                <v-flex sm6 pb-3 pl-2 min-height>
-                  <v-card>
-                    <v-card-text>report status</v-card-text>
-                  </v-card>
-                </v-flex>
-                <v-flex sm12 pb-3>
-                  <v-card>
-                    <v-card-text>perfomance</v-card-text>
-                  </v-card>
-                </v-flex>
-                <v-flex sm12 pb-3>
-                  <v-card>
-                    <v-card-text>calendar</v-card-text>
-                  </v-card>
-                </v-flex>
-              </v-layout>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-card>
+    <div>
+      <Navigation/>
+      <div class="pa-3">
+        <v-card flat min-height="95vh" class="transparent">
+          <v-layout row wrap>
+            <v-flex sm2>
+              <Sidenav/>
+            </v-flex>
+            <v-flex sm10>
+              <v-card class="transparent" flat>
+                <v-layout row wrap>
+                  <v-flex sm5 pb-3 pr-2> 
+                    <v-card min-height="40vh">
+                      <v-card-text align-center>
+                        <VivaStatus/>
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex sm7 pb-3 pl-2 min-height="">   
+                    <v-card>
+                      <v-card-text>
+                        report status
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex sm12 pb-3>
+                    <v-card>
+                      <v-card-text>
+                        perfomance
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex sm12 pb-3>
+                    <v-card>
+                      <v-card-text>
+                        calendar
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Navigation from "@/components/Navbar.vue";
-import Sidenav from "@/components/Sidenav.vue";
+import Navigation from "@/components/Navbar.vue"
+import Sidenav from "@/components/Sidenav.vue"
+import VivaStatus from "@/components/VivaStatus.vue"
 export default {
-  name: "dashboard",
-  components: {
-    Navigation,
-    Sidenav
-  }
-};
+    name: 'dashboard',
+    components: {
+        Navigation,
+        Sidenav,
+        VivaStatus
+    },
+}
 </script>
 
 <style lang="scss">
