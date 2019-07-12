7395d5042546720527720a2840f9391c850e1f57
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-12 12:41:43 +0300

    [Wycliff] removes calendar from side nav

--- src/components/Sidenav.vue
+++ src/components/Sidenav.vue
@@ -18,20 +18,15 @@
         </v-toolbar>
         <v-divider></v-divider>
         <v-list dense>
-        <v-list-tile
-            v-for="item in items"
-            :key="item.title"
-            v-on:click=""
-            >
-            <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile fixed>
-            <v-list-tile-content>
-                <v-list-tile-title>Logout</v-list-tile-title>
-            </v-list-tile-content>
-        </v-list-tile>
+            <v-list-tile
+                v-for="item in items"
+                :key="item.title"
+                v-on:click=""
+                >
+                <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
         </v-list>
     </v-navigation-drawer>
 </template>
@@ -42,7 +37,7 @@ export default {
       return {
         items: [
           { title: 'Notifications'},
-          { title: 'Calendar'},
+          { title: 'logout'},
         ],
       }
     }
