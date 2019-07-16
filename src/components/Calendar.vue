b479eb7c62842e15e513eed7174443423105ac92
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-16 11:51:47 +0300

    [Wycliff] refactors calendar component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,40 +1,37 @@
 <template>
   <v-layout row wrap pa-3>
     <v-flex xs12 sm12 md4>
-      <v-date-picker
-        ref="picker"
-        v-model="date"
-        full-width
-        :picker-date.sync="pickerDate"
-        :event-color="date => date[9] % 2 ? 'red' : 'green'"
-        :events="eventsFunction"
-      ></v-date-picker>
+        <v-date-picker
+          v-model="date"
+          full-width
+          :events="eventDatesArr"
+          event-color="green lighten-1"
+        ></v-date-picker>
     </v-flex>
     <v-flex xs12 sm12 md4 pl-3>
-      <v-card height="100%">
-        <v-container fluid pa-0>
-          <v-toolbar color="blue darken-2" dark height="89" flat>
-            <v-toolbar-title>Date Events: ({{ selectedDate }})</v-toolbar-title>
-          </v-toolbar>
-          <ul class="ma-3">
-            <li v-for="note in notes" :key="note">{{ note }}</li>
-          </ul>
-        </v-container>
-      </v-card>
+        <v-card height="100%">
+          <v-container fluid pa-0>
+            <v-toolbar color="blue darken-2" dark height="89" flat>
+              <v-toolbar-title>Date Events: ({{ selectedDate }})</v-toolbar-title>
+            </v-toolbar>
+            <!-- <ul class="ma-3">
+              <li v-for="note in notes" :key="note">{{ note }}</li>
+            </ul> -->
+          </v-container>
+        </v-card>
     </v-flex>
     <v-flex xs12 sm12 md4 pl-1>
-      <v-card height="100%">
-        <v-container fluid pa-0>
-          <v-toolbar color="blue darken-2" dark height="89" flat>
-            <v-toolbar-title>Event Details</v-toolbar-title>
-            <v-spacer></v-spacer>
-          </v-toolbar>
-          <div class="subheading">Event title</div>
-          <ul class="ma-3">
-            <li v-for="note in notes" :key="note">{{ note }}</li>
-          </ul>
-        </v-container>
-      </v-card>
+        <v-card height="100%">
+          <v-container fluid pa-0>
+            <v-toolbar color="blue darken-2" dark height="89" flat>
+              <v-toolbar-title>Event Details</v-toolbar-title>
+            </v-toolbar>
+            <div class="subheading">Event title</div>
+            <!-- <ul class="ma-3">
+              <li v-for="note in notes" :key="note">{{ note }}</li>
+            </ul> -->
+          </v-container>
+        </v-card>
     </v-flex>
   </v-layout>
 </template>
@@ -46,48 +43,19 @@ export default {
   data: () => ({
     date: new Date().toISOString().substr(0, 10),
     selectedDate: '',
-    pickerDate: null,
-    notes: [],
-    events: [],
-    allNotes: [
-      "President met with prime minister",
-      "New power plant opened",
-      "Rocket launch announced",
-      "Global warming discussion cancelled",
-      "Company changed its location"
-    ]
+    eventDatesArr: null,
   }),
+
   updated() {
     this.events = CalendarEvents.getEventsByDate(this.date);
     this.selectedDate = this.date
-    // console.log("The date is ", this.date);
-  },
-  watch: {
-    pickerDate(val) {
-      this.notes = [
-        this.allNotes[Math.floor(Math.random() * 5)],
-        this.allNotes[Math.floor(Math.random() * 5)],
-        this.allNotes[Math.floor(Math.random() * 5)]
-      ].filter((value, index, self) => self.indexOf(value) === index);
-    }
   },
 
   methods: {
-    eventsFunction(date) {
-      const [, , day] = date.split("-");
-      if ([12, 17, 28].includes(parseInt(day, 10))) return true;
-      if ([1, 19, 22].includes(parseInt(day, 10))) return ["red", "#00f"];
-      return false;
-    }
   },
 
   mounted() {
-    this.arrayEvents = [...Array(6)].map(() => {
-      const day = Math.floor(Math.random() * 30);
-      const d = new Date();
-      d.setDate(day);
-      return d.toISOString().substr(0, 10);
-    });
+    this.eventDatesArr = CalendarEvents.eventDates
   }
 };
 </script>
