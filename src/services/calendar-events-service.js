e28f5357fb80bfca30439bd054164ea6102f1f51
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-16 11:51:01 +0300

    [Wycliff] creates event dates array

--- src/services/calendar-events-service.js
+++ src/services/calendar-events-service.js
@@ -1,26 +1,30 @@
+
+let events = [
+	{
+		title: "Event one",
+		content: "Event One's content",
+		date: "2019-07-16"
+	},
+	{
+		title: "Event Two",
+		content: "Event Two's content",
+		date: "2019-07-17"
+	},
+	{
+		title: "Event Three",
+		content: "Event One's content",
+		date: "2019-07-18"
+	}
+];
+let eventDates = events.map(event => event.date)
+
 export default {
 	getEventsByDate(calendarDate) {
-		let events = [
-			{
-				title: "Event one",
-				content: "Event One's content",
-				date: "2019-07-16"
-			},
-			{
-				title: "Event Two",
-				content: "Event Two's content",
-				date: "2019-07-17"
-			},
-			{
-				title: "Event Three",
-				content: "Event One's content",
-				date: "2019-07-18"
-			}
-		];
 		let eventsOnADay = events.filter(event => {
 			return event.date == calendarDate;
 		});
 		console.log("The events on this day are: ", eventsOnADay);
 		return eventsOnADay;
-	}
+	},
+	eventDates
 };
