aa9e66c908c285ae0bfddd554f9ac2f5d5ec4e68
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-07-17 15:42:24 +0300

    [Wycliff/Rodrick] Fixes merge conflict after rebase

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -1,25 +1,23 @@
 <template>
-    <div>
-      <Navigation/>
-      <v-card flat min-height="600px">
-        <Sidenav/>
-      </v-card>
-    </div>
+  <div>
+    <Navigation />
+    <v-card flat min-height="600px">
+      <Sidenav />
+    </v-card>
+  </div>
 </template>
 
 <script>
-import Navigation from "@/components/Navbar.vue"
-import Sidenav from "@/components/Sidenav.vue"
+import Navigation from "@/components/Navbar.vue";
+import Sidenav from "@/components/Sidenav.vue";
 export default {
-    name: 'dashboard',
-    components: {
-        Navigation,
-        Sidenav,
-    },
-}
+  name: "dashboard",
+  components: {
+    Navigation,
+    Sidenav
+  }
+};
 </script>
 
 <style lang="scss">
-
-
 </style>
