f6a5a5164c90b9b2f9de8cbd3a95d8544fd71f95
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-17 11:55:15 +0300

    [Wycliff] creates departments-service

---
+++ src/departments-service.js
@@ -0,0 +1,32 @@
+let ECEdepartments=[
+        {
+            name: 'Electrical and Computer',
+            programs: [
+                {
+                    name: "Masters in Computer engineering"
+                },
+                {
+                    name: "Masters in Telecommunications engineering"
+                }
+            ]
+        },
+        {
+            name: 'Civil and Environmental',
+            programs: [
+                {
+                    name: "Masters in Civil engineering"
+                }
+            ]
+        },
+        {
+            name: 'Mechanical',
+            programs: [
+                {
+                    name: "Masters in Mechanical engineering"
+                }
+            ]
+        }
+    ]
+export default {
+    ECEdepartments
+}
