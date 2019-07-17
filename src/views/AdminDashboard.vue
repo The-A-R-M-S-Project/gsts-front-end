6165261c1b069fac738418bb0b8c5fdb8a1340fc
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-07-17 15:47:52 +0300

    Fixes merge conflict

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -1,9 +1,41 @@
 <template>
   <div>
     <Navigation />
-    <v-card flat min-height="600px">
-      <Sidenav />
-    </v-card>
+    <div class="pa-3">
+      <v-card flat min-height="95vh" class="transparent">
+        <v-layout row wrap>
+          <v-flex>
+            <Sidenav />
+          </v-flex>
+          <v-flex sm10>
+            <v-card class="transparent" flat>
+              <v-layout row wrap>
+                <v-flex sm6 pb-3 pr-2>
+                  <v-card>
+                    <v-card-text>viva status</v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex sm6 pb-3 pl-2 min-height>
+                  <v-card>
+                    <v-card-text>report status</v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex sm12 pb-3>
+                  <v-card>
+                    <v-card-text>perfomance</v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex sm12 pb-3>
+                  <v-card>
+                    <v-card-text>calendar</v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </div>
   </div>
 </template>
 
