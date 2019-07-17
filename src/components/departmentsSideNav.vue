5daeb7d58112f7863f3730508287f70c06641453
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-17 11:54:47 +0300

    [Wycliff] creates departments side nav component

---
+++ src/components/departmentsSideNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-navigation-drawer stateless value="true">
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>Departments</v-list-tile-title>
+      </v-list-tile>
+
+      <v-list-group prepend-icon="" active-class :value="false" v-for="(department, i) in departments" :key="i">
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-title>{{ department.name }}</v-list-tile-title>
+            </v-list-tile>
+          </template>
+
+          <v-list-tile v-for="(program, i) in department.programs" :key="i" @click="">
+            <v-icon small light>star</v-icon>
+            <v-list-tile-action style="font-size: 12px;">{{ program.name }}</v-list-tile-action>
+          </v-list-tile>
+
+        </v-list-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import Departments from "@/departments-service.js"
+export default {
+  data() {
+      return{
+        departments: Departments.ECEdepartments
+      }
+  }
+}
+</script>
+
