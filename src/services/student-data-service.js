d220ecf59691fb4fa03b2c8dd514a8032f611a4d
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-17 22:05:39 +0300

    Moves dummy data to the services folder

--- src/services/student-data-service.js
+++ src/services/student-data-service.js
@@ -1,65 +1,65 @@
 let studentData = [
   {
     name: "Student1",
-    regNo: "15/U/56732",
-    reportStatus: "With examiner",
-    vivaStatus: "done",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student2",
-    regNo: "18/U/52267",
-    reportStatus: "With examiner",
-    vivaStatus: "done",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student3",
-    regNo: "15/U/56700",
-    reportStatus: "With examiner",
-    vivaStatus: "pending",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student4",
-    regNo: "17/U/3567",
-    reportStatus: "With examiner",
-    vivaStatus: "done",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student5",
-    regNo: "16/U/5657",
-    reportStatus: "With examiner",
-    vivaStatus: "done",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student6",
-    regNo: "18/U/5167",
-    reportStatus: "With examiner",
-    vivaStatus: "pending",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student7",
-    regNo: "15/U/567",
-    reportStatus: "With examiner",
-    vivaStatus: "done",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student8",
-    regNo: "16/U/567",
-    reportStatus: "With examiner",
-    vivaStatus: "done",
-    vivaDate: "14-07-2019"
-  },
-  {
-    name: "Student9",
     regNo: "18/U/567",
     reportStatus: "With examiner",
-    vivaStatus: "pending",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
+    vivaDate: "14-07-2019"
+  },
+  {
+    name: "Student1",
+    regNo: "18/U/567",
+    reportStatus: "With examiner",
+    vivaStatus: "done",
     vivaDate: "14-07-2019"
   },
 ];
