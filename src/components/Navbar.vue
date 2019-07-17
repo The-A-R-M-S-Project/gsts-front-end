41d0c63fe56aa03b8491b673ab49966cd02cab5b
Ben-Cliff <benclifg@gmail.com>
2019-07-17 21:20:00 +0300

    Adds route paths to navigation links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,8 +5,8 @@
         G.S.T.S
       </v-toolbar-title>
       <div>
-        <v-btn flat small>Dashboard</v-btn>
-        <v-btn flat small>Departments</v-btn>
+        <v-btn flat small to="/dashboard">Dashboard</v-btn>
+        <v-btn flat small to="/departments">Departments</v-btn>
         <v-btn flat small>Examiners</v-btn>  
       </div>
     </v-toolbar>
