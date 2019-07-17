2cf8c1d24f4f74899fa4b96404681675717ef366
Ben-Cliff <benclifg@gmail.com>
2019-07-17 08:26:34 +0300

    [Rodrick] creates departments view

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
-import Dashboard from "./views/AdminDashboard.vue";
 import Departments from "./views/Departments.vue"
 
 
@@ -19,11 +18,6 @@ export default new Router({
       path: "/register",
       name: "register",
       component: Register
-    },
-    {
-      path: "/dashboard",
-      name: "dasboard",
-      component: Dashboard
     },
     {
       path: "/departments",
