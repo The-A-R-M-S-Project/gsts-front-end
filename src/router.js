ee41bd16d076001f1c32dda1eb0548b6de581233
Ben-Cliff <benclifg@gmail.com>
2019-07-17 08:24:51 +0300

    [Rodrick] adds departments route

--- src/router.js
+++ src/router.js
@@ -2,26 +2,27 @@ import Vue from "vue";
 import Router from "vue-router";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
-import AdminDashboard from "./views/AdminDashboard.vue";
+import Departments from "./views/Departments.vue"
+
 
 Vue.use(Router);
 
 export default new Router({
-	routes: [
-		{
-			path: "/",
-			name: "login",
-			component: Login
-		},
-		{
-			path: "/register",
-			name: "register",
-			component: Register
-		},
-		{
-			path: "/admin-dashboard",
-			name: "admin-dasboard",
-			component: AdminDashboard
-		}
-	]
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
+    },
+    {
+      path: "/departments",
+      name: "departments",
+      component: Departments
+    }
+  ]
 });
