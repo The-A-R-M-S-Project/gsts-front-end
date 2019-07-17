eed66a64a6aa1b32f0d547601c159c477bc7eb14
Ben-Cliff <benclifg@gmail.com>
2019-07-17 21:01:33 +0300

    Adds fixed navigation bar

--- src/views/Departments.vue
+++ src/views/Departments.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        <!-- <Navigation/> -->
-        <v-layout row wrap>
-            <v-flex md3 pt-4>
+        <div class="navbar">
+            <Navigation/>
+        </div>
+        <v-layout row wrap pt-4>
+            <v-flex md3 pt-5>
                 <SideNav @clickedLink="setProgram" @clickedDepartment="setDepartment"></SideNav>
             </v-flex>
-            <v-flex md9 style="height: 95vh;" class="scroll-y">
+            <v-flex md9 pt-5 style="height: 90vh;" class="scroll-y">
                 <v-container>
                     <h1 class="text-xs-center">{{ department+' Engineering'}}</h1>
                     <StudentsTable :program="program"></StudentsTable>
@@ -15,15 +17,21 @@
     </div>
 </template>
 <style>
-
+.navbar{
+    position: fixed; 
+    z-index: 1; 
+    width: 100%;
+}
 </style>
 <script>
 import StudentsTable from "@/components/StudentsTable.vue"
 import SideNav from "@/components/departmentsSideNav.vue"
+import Navigation from "@/components/Navbar.vue"
     export default {
         components: {
             StudentsTable,
-            SideNav
+            SideNav,
+            Navigation
         },
         data(){
             return{
