3752f9647ca46f5bced78c6cf6dd594b11653201
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-17 08:52:28 +0300

    [Wycliff] creates Students' Table component

--- src/views/Departments.vue
+++ src/views/Departments.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-        departments view
+        <!-- <Navigation/> -->
+        <v-layout row wrap>
+            <v-flex md2>
+                <!-- <Sidenav/> -->
+            </v-flex>
+            <v-flex md10>
+                <StudentsTable/>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
 
 <script>
+import StudentsTable from "@/components/StudentsTable.vue"
     export default {
 
     }
