28a0b9b289be4e1e883bc877092fe5aae848af3c
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-07-18 10:02:55 +0300

    [alvin] Removes strict password requirements on login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,43 +1,56 @@
 <template>
-    <v-form ref="form" v-model="form">
-      <v-container>
-           <v-text-field
-                v-model="email"
-                :rules="[rules.email]"
-                label="College Email"
-                prepend-inner-icon="person"
-                type="email"
-           >
-           </v-text-field>
-            <v-text-field
-                v-model="password"
-                :rules="[rules.password, rules.length(8)]"
-                counter="8"
-                label="password"
-                prepend-inner-icon="lock"
-                type="password"
-            >
-            </v-text-field>
-            <div class="text-md-left">
-                <v-btn round large depressed ripple class="yellow accent-1">Login</v-btn>
-            </div>
-      </v-container>
-    </v-form>
+  <v-form ref="form" v-model="form" class="login-form" name="login">
+    <v-container>
+      <v-text-field
+        v-model="email"
+        :rules="[rules.email]"
+        label="College Email"
+        prepend-inner-icon="person"
+        type="email"
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :rules="[rules.length(8)]"
+        counter="8"
+        label="password"
+        prepend-inner-icon="lock"
+        type="password"
+      ></v-text-field>
+      <div class>
+        <v-btn
+          round
+          large
+          depressed
+          ripple
+          class="yellow accent-1"
+          type="submit"
+          v-on:click="submitForm"
+        >Login</v-btn>
+      </div>
+    </v-container>
+  </v-form>
 </template>
 <script>
-export default{
-    name: "LoginForm",
-    data: () => ({
-      email: undefined,
-      form: false,
-      password: undefined,
-      rules: {
-        email: v => (v || '').match(/@cedat.mak.ac.ug/) || 'Please enter a valid college email',
-        length: len => v => (v || '').length >= len || `Invalid character length, required ${len}`,
-        password: v => (v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
-          'Must contain an uppercase letter, a numeric & special characters',
-        required: v => !!v || 'This field is required'
-      }
-    })
-}
+export default {
+  name: "LoginForm",
+  data: () => ({
+    email: undefined,
+    form: false,
+    password: "",
+    rules: {
+      email: v =>
+        (v || "").match(/@cedat.mak.ac.ug/) ||
+        "Please enter a valid college email",
+      length: len => v =>
+        (v || "").length >= len || `Invalid character length, required ${len}`,
+      required: v => !!v || "This field is required"
+    }
+  }),
+  methods: {
+    submitForm() {
+      console.log("Submitted");
+      // document.getElementsByClassName("login-form");
+    }
+  }
+};
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,78 +1,78 @@
 <template>
   <v-container text-xs-center fluid fill-height class="login">
     <v-layout align-center justify-center>
-        <v-card min-width="90%" class="elevation-24">
-          <v-layout row wrap height>
-            <v-flex sm7 pa-3>
-              <v-card-text>
-                <v-container class="text-md-left">
-                  <h2 class="headline text-md-left pl-md-5">Login</h2>
-                  <v-tabs
-                    v-model="tabs"
-                    fixed-tabs
-                    flat
-                  >
-                    <v-tabs-slider></v-tabs-slider>
-                    <v-tab href="#mobile-tabs-5-1">
-                      <p>Admin</p>
-                    </v-tab>
-          
-                    <v-tab href="#mobile-tabs-5-2">
-                      <p>Student</p>
-                    </v-tab>
-                  </v-tabs>
-                  <v-tabs-items v-model="tabs" class="white elevation-0">
-                    <v-tab-item
-                      v-for="i in 2"
-                      :key="i"
-                      :value="'mobile-tabs-5-' + i"
-                    >
-                        <LoginForm/>
-                    </v-tab-item>
-                  </v-tabs-items>
-                </v-container>
-              </v-card-text>
-            </v-flex>
-            <v-flex sm5>
-              <v-container fill-height pa-0 overlayed>
-                <v-layout align-center>
-                  <v-flex px-4 py-3>
-                      <h1 class="display-2 font-weight-light mb-4">Graduate Student Tracking System</h1>
-                      <p>A system for tracking master's students reports</p>
-                      <p class="subheading">Are you a student? Register Here!</p>
-                      <v-btn round large depressed block to="/register" class="grey login__register-btn">
-                        <v-icon>person_add</v-icon>
-                        <span>&nbsp;Register</span>
-                      </v-btn>
-                  </v-flex>
-                </v-layout>   
+      <v-card min-width="90%" class="elevation-24">
+        <v-layout row wrap height>
+          <v-flex sm7 pa-3>
+            <v-card-text>
+              <v-container class>
+                <h2 class="headline pl-md-5">Login</h2>
+                <v-tabs v-model="tabs" fixed-tabs flat>
+                  <v-tabs-slider></v-tabs-slider>
+                  <v-tab href="#mobile-tabs-5-1">
+                    <p>Admin</p>
+                  </v-tab>
+
+                  <v-tab href="#mobile-tabs-5-2">
+                    <p>Student</p>
+                  </v-tab>
+                </v-tabs>
+                <v-tabs-items v-model="tabs" class="white elevation-0">
+                  <v-tab-item v-for="i in 2" :key="i" :value="'mobile-tabs-5-' + i">
+                    <LoginForm />
+                  </v-tab-item>
+                </v-tabs-items>
               </v-container>
-            </v-flex>     
-          </v-layout>
-        </v-card>
+            </v-card-text>
+          </v-flex>
+          <v-flex sm5>
+            <v-container fill-height pa-0 overlayed>
+              <v-layout align-center>
+                <v-flex px-4 py-3>
+                  <h1 class="display-2 font-weight-light mb-4">Graduate Student Tracking System</h1>
+                  <p>A system for tracking master's students reports</p>
+                  <p class="subheading">Are you a student? Register Here!</p>
+                  <v-btn
+                    round
+                    large
+                    depressed
+                    block
+                    to="/register"
+                    class="grey login__register-btn"
+                  >
+                    <v-icon>person_add</v-icon>
+                    <span>&nbsp;Register</span>
+                  </v-btn>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  import LoginForm from "@/components/LoginForm"
-  export default {
-    components: {
-      LoginForm,
-    },
-    data(){
-      return{
-        tabs: null,
-      }
-    }
+import LoginForm from "@/components/LoginForm";
+export default {
+  components: {
+    LoginForm
+  },
+  data() {
+    return {
+      tabs: null
+    };
   }
+};
 </script>
 <style lang="scss">
 .overlayed {
   width: 100%;
   height: 100%;
   background-size: cover;
-  background-image: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url("~@/assets/img/login-2.jpg");
+  background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
+    url("~@/assets/img/login-2.jpg");
   position: relative;
   color: white;
   background-position: center;
@@ -81,13 +81,13 @@
   font-size: 18px;
   letter-spacing: 150%;
 }
-.overlayed h1{
+.overlayed h1 {
   font-size: 25px;
   font-weight: lighter;
 }
 .login {
   &__register-btn {
-    opacity: .65;
+    opacity: 0.65;
     width: 80%;
     margin: auto;
     font-weight: bold;
