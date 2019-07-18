1d72b5d60c6aa7061b7387228241ac99a4087cb6
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-18 08:46:40 +0300

    [Wycliff] Creates Student dashboard view

---
+++ src/views/StudentDashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="navbar">
+        <Navigation/>
+    </div>
+    <div class="scroll-y pt-5">
+        <Timeline/>
+    </div>
+  </div>
+</template>
+<script>
+import Navigation from "@/components/Navbar.vue"
+import Timeline from "@/components/Timeline.vue"
+export default {
+  name: "studentview",
+  components: {
+    Timeline,
+    Navigation,
+  },
+};
+</script>
