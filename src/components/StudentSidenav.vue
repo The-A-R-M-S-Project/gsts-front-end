61e81692dc2b52929539c97de6a9427fa4b4d873
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-18 09:11:11 +0300

    [Wycliff] renames admin's view for student timeline

---
+++ src/components/StudentSidenav.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-navigation-drawer stateless value="true" style="height: 89vh;">
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>Departments</v-list-tile-title>
+      </v-list-tile>
+
+      <v-list-group prepend-icon="" active-class :value="false" v-for="(department, i) in departments" :key="i" v-on:click="clickedDepartment(department.name)">
+          <template v-slot:activator>
+            <v-list-tile >
+              <v-list-tile-title>{{ department.name }}</v-list-tile-title>
+            </v-list-tile>
+          </template>
+
+          <v-list-tile v-for="(program, i) in department.programs" :key="i">
+            <v-icon small light>star</v-icon>
+            <v-list-tile-action style="font-size: 12px;" v-on:click="clickedLink(program.name)">{{ program.name }}</v-list-tile-action>
+          </v-list-tile>
+
+        </v-list-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import Departments from "@/services/departments-service.js"
+export default {
+  data() {
+      return{
+        departments: Departments.ECEdepartments
+      }
+  },
+  methods: {
+      clickedLink: function(programName){
+          this.$emit('clickedLink', programName)
+      },
+      clickedDepartment: function(departmentName){
+        this.$emit('clickedDepartment', departmentName)
+      }
+  }
+}
+</script>
+
