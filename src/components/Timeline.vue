ccd8a450c3a0aef03ccb9f27f2bae1862f36450b
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-18 15:39:38 +0300

    [Wycliff] adds examiner returned report step

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,64 +1,77 @@
 <template>
-  <v-stepper v-model="e6" vertical>
-    <v-stepper-step :complete="e6 > 1" step="1">
-      Report handed In
-      <small>Report handed to Deputy Principal by Student {{' '+date}}</small>
-    </v-stepper-step>
+  <div>
+    <div v-if="!cleared">
+      <v-stepper v-model="e6" vertical>
+        <v-stepper-step :complete="e6 > 1" step="1">
+          Report handed In
+          <small>Report handed to Deputy Principal by Student {{ setDate() }}</small>
+        </v-stepper-step>
 
-    <v-stepper-content step="1">
-      <v-btn color="primary" @click="e6 = 2, setDate()">Report Submitted</v-btn>
-      <v-btn flat>Cancel</v-btn>
-    </v-stepper-content>
+        <v-stepper-content step="1">
+          <v-btn color="primary" @click="e6 = 2, setDate()">Report Submitted</v-btn>
+          <v-btn flat>Cancel</v-btn>
+        </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 2" step="2">
-      Report sent to examiner
-      <small>Report forwarded to examiner by Deputy Principal</small>
-    </v-stepper-step>
+        <v-stepper-step :complete="e6 > 2" step="2">
+          Report sent to examiner
+          <small>Report forwarded to examiner by Deputy Principal {{' '+date}}</small>
+        </v-stepper-step>
 
-    <v-stepper-content step="2">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="e6 = 3, setDate()">Continue</v-btn>
-      <v-btn flat @click="e6 = 1">Cancel</v-btn>
-    </v-stepper-content>
+        <v-stepper-content step="2">
+          <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click="e6 = 3, setDate()">Continue</v-btn>
+          <v-btn flat @click="e6 = 1">Cancel</v-btn>
+        </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 3" step="3">
-      Report received by examiner
-      <small></small>
-    </v-stepper-step>
+        <v-stepper-step :complete="e6 > 3" step="3">
+          Report received by examiner
+          <small>{{' '+date}}</small>
+        </v-stepper-step>
 
-    <v-stepper-content step="3">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="examinerCleared()">Continue</v-btn>
-      <v-btn flat @click="e6 = 2">Cancel</v-btn>-->
-    </v-stepper-content>
+        <v-stepper-content step="3">
+          <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click="examinerCleared()">Continue</v-btn>
+          <v-btn flat @click="e6 = 2">Cancel</v-btn>
+        </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 4" step="4">
-      Report received by examiner
-      <small></small>
-    </v-stepper-step>
+        <v-stepper-step :complete="e6 > 4" step="4">
+          Report returned by examiner
+          <small>{{' '+date}}</small>
+        </v-stepper-step>
 
-    <v-stepper-content step="4">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="examinerReturnedMark()">Continue</v-btn>
-      <v-btn flat @click="e6 = 3">Cancel</v-btn>-->
-    </v-stepper-content>
+        <v-stepper-content step="4">
+          <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click="e6=5">Continue</v-btn>
+          <v-btn flat @click="e6 = 3">Cancel</v-btn>
+        </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 5" step="5">Viva date set</v-stepper-step>
+        <v-stepper-step :complete="e6 > 5" step="5">
+          Viva date set
+          <small>{{' '+date}}</small>
+        </v-stepper-step>
 
-    <v-stepper-content step="5">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="e6 = 6">set date</v-btn>
-      <v-btn flat @click="e6 = 4">Cancel</v-btn>
-    </v-stepper-content>
+        <v-stepper-content step="5">
+          <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click="e6 = 6">set date</v-btn>
+          <v-btn flat @click="e6 = 4">Cancel</v-btn>
+        </v-stepper-content>
 
-    <v-stepper-step step="6">Viva pending</v-stepper-step>
+        <v-stepper-step step="6">
+          Viva pending
+          <small>{{' '+date}}</small>
+        </v-stepper-step>
 
-    <v-stepper-content step="5">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click>Done</v-btn>
-      <v-btn flat @click="e6 = 5">Cancel</v-btn>
-    </v-stepper-content>
-  </v-stepper>
+        <v-stepper-content step="6">
+          <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click="vivaDone()">Done</v-btn>
+          <v-btn flat @click="e6 = 5">Cancel</v-btn>
+        </v-stepper-content>
+      </v-stepper>
+    </div>
+    <div v-if="cleared">
+
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -66,7 +79,9 @@ export default {
     return {
       e6: 1,
       examiner: true,
-      date: ""
+      reportMark: true,
+      date: "",
+      cleared: false
     };
   },
   methods: {
@@ -74,12 +89,16 @@ export default {
       this.examiner ? (this.e6 = 4) : (this.e6 = 3);
     },
     setDate() {
-      this.date =
+      return (
         "on " +
         new Date().toLocaleDateString() +
         " " +
-        new Date().toLocaleTimeString();
-    }
+        new Date().toLocaleTimeString()
+      );
+    },
+    vivaDone() {
+        this.cleared = true
+    },
   }
 };
 </script>
