dec0984f751b7c6121ef7be990fcf336726b68bf
Ben-Cliff <benclifg@gmail.com>
2019-07-19 09:44:32 +0300

    Creates DatePicker component

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-date-picker v-model="picker" :landscape="landscape"></v-date-picker>
+</template>
+<script>
+export default {
+    props: {
+        landscape: Boolean
+    },
+    data () {
+    return {
+    picker: new Date().toISOString().substr(0, 10),
+    }
+    }
+}
+</script>
+
