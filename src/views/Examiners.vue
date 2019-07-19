8d5c9d7d46e3cb2860d0c767d3b8468001ad96c8
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-19 09:50:54 +0300

    [Wycliff] Adds Navbar and sidenav to examiners' view

--- src/views/Examiners.vue
+++ src/views/Examiners.vue
@@ -1,8 +1,46 @@
 <template>
-    
+    <div>
+        <div class="navbar">
+            <Navigation/>
+        </div>
+        <v-layout row pt-4>
+            <v-flex md3 pt-5>
+                <SideNav @clickedLink="setProgram" @clickedDepartment="setDepartment"></SideNav>
+            </v-flex>
+            <v-flex md9 pt-5 style="height: 95vh;" class="scroll-y">
+                <v-container>
+                    <h1 class="text-xs-center">{{ department+' Engineering'}}</h1>
+                    <StudentsTable :program="program"></StudentsTable>
+                </v-container>
+            </v-flex>
+        </v-layout>
+    </div>
 </template>
+<style>
+
+</style>
 <script>
-export default {
-    
-}
+import SideNav from "@/components/departmentsSideNav.vue"
+import Navigation from "@/components/Navbar.vue"
+    export default {
+        components: {
+            SideNav,
+            Navigation
+        },
+        data(){
+            return{
+                program: null,
+                department: 'Electrical and Computer'
+            }
+        },
+        methods: {
+            setProgram(programName){
+                this.program = programName
+            },
+            setDepartment(departmentName){
+                this.department = departmentName
+            }
+        }
+        
+    }
 </script>
