47b2ff325f5ebe752d1638a2a4b75da1824a0d44
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-19 11:06:08 +0300

    [Wycliff] Creates examiners' dummy data

---
+++ src/services/examiners-data-service.js
@@ -0,0 +1,75 @@
+let examinerData = [
+  {
+    name: "Examiner1",
+    department: "Electrical and Computer Engineering",
+    category: "internal",
+    offNo: "Rm 050",
+    contact: "+256770459045",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner2",
+    department: "Civil and Environmental",
+    category: "internal",
+    offNo: "Rm 001",
+    contact: "+256783875383",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner3",
+    department: "Electrical and Computer Engineering",
+    category: "external",
+    offNo: "Rm 100",
+    contact: "+256757834094",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner4",
+    department: "Mechanical Engineering",
+    category: "external",
+    offNo: "Rm 011",
+    contact: "+256765325645",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner5",
+    department: "Mechanical Engineering",
+    category: "external",
+    offNo: "Rm 101",
+    contact: "+1256773277553",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner6",
+    department: "Civil and Environmental",
+    category: "internal",
+    offNo: "Rm 111",
+    contact: "+256783738463",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner7",
+    department: "Electrical and Computer Engineering",
+    category: "external",
+    offNo: "Rm 1003",
+    contact: "+254743493843",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner8",
+    department: "Mechanical Engineering",
+    category: "internal",
+    offNo: "Rm 3453",
+    contact: "+255767368857",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+  {
+    name: "Examiner9",
+    department: "Civil and Environmental",
+    category: "external",
+    offNo: "Rm 1223",
+    contact: "+256724752742",
+    email: "examiner@cedat.mak.ac.ug"
+  },
+];
+export default examinerData;
