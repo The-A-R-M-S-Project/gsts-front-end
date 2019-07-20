6513038ef85134bf7e3943b837d5c36c5ddfd19f
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-07-20 13:40:11 +0300

    Sets the default Vue instance $http object to use Axios

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-import Vue from "vue"
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import './plugins/vuetify'
-import App from "./App.vue"
-import router from "./router"
-import store from "./store"
-import "./registerServiceWorker"
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import Axios from "axios";
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
+Vue.prototype.$http = Axios;
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount("#app");
