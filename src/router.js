e73f5c02da29dcd1f16a7517584bd6c075e31b73
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-07-21 13:01:02 +0300

    Creates a dummy student dashboard view for testing authentication

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
 import AdminDashboard from "./views/AdminDashboard.vue";
+import DummyStudentDashboard from "./views/DummyStudentDashboard.vue";
 
 Vue.use(Router);
 
@@ -11,17 +12,35 @@ export default new Router({
 		{
 			path: "/",
 			name: "login",
-			component: Login
+			component: Login,
+			meta: {
+				guest: true
+			}
 		},
 		{
 			path: "/register",
 			name: "register",
-			component: Register
+			component: Register,
+			meta: {
+				guest: true
+			}
+		},
+		{
+			path: "/student-dashboard",
+			name: "student-dasboard",
+			component: DummyStudentDashboard,
+			meta: {
+				requiresAuth: true
+			}
 		},
 		{
 			path: "/admin-dashboard",
 			name: "admin-dasboard",
-			component: AdminDashboard
+			component: AdminDashboard,
+			meta: {
+				requiresAuth: true,
+				is_Admin: true
+			}
 		}
 	]
 });
---
+++ src/views/DummyStudentDashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>THis is a Dummy Student Dashbord to be used temporariry in implementing authentication,</h1>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "dummy-student-dashboard"
+};
+</script>
