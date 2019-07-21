d221e6dc74ca0655a9143d4db67c0e07db8d415d
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-07-21 15:29:09 +0300

    [Alvin] Creates dummy view components for new user roles"

---
+++ src/views/DummyExaminerDashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Dummy examiner Dashboard</h1>
+    <p>This is a dummy examiner Dash</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dummy-examiner-dashboard"
+};
+</script>
