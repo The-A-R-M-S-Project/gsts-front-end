193802d729c1f53cad53215a34ee6b2ad01082a1
rodrickcalvin <rodrickwamala@gmail.com>
2019-07-22 23:18:55 +0300

    Makes Sidenav dark

--- src/components/ExaminersSidenav.vue
+++ src/components/ExaminersSidenav.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer stateless value="true" style="height: 89vh;">
+  <v-navigation-drawer dark value="true" style="height: 137vh;">
     <v-toolbar flat style="display: flex;" class="transparent justify-center pl-0" height="220">
         <v-avatar color="blue grey lighten-2" size="200">
             <v-icon dark size="150px">person</v-icon>
