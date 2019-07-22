06f71fc081f1e684315c817f712f86b6f5031082
Ben-Cliff <benclifg@gmail.com>
2019-07-22 20:53:52 +0300

    Adds student profile component

--- src/views/StudentDashboard.vue
+++ src/views/StudentDashboard.vue
@@ -6,13 +6,13 @@
     <div class="pt-5">
       <v-layout row pt-4>
         <v-flex md3>
-          <SideNav/>
+          <SideNav @clickedProfile="setTimelineRoute"/>
         </v-flex>
         <v-flex v-if="route===`timeline`" md9 style="height: 88vh;" class="scroll-y">
 
         </v-flex>
         <v-flex v-else md9 style="height: 88vh;" class="scroll-y">
-
+          <StudentProfile/>
         </v-flex>
       </v-layout>
     </div>
@@ -22,11 +22,24 @@
 <script>
 import Navigation from "@/components/StudentsNav.vue"
 import SideNav from "@/components/StudentSidenav.vue"
+import StudentProfile from "@/components/StudentProfile.vue"
+
 export default {
   name: "student-dashboard",
   components: {
     Navigation,
-    SideNav
+    SideNav,
+    StudentProfile
+  },
+  data() {
+    return {
+      route: "timeline"
+    }
+  },
+  methods: {
+    setTimelineRoute(route) {
+      this.route = route
+    }
   }
 };
 </script>
