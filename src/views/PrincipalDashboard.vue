0353403a0739d5522ca9a40509d69ca6a5ad4925
Ben-Cliff <benclifg@gmail.com>
2019-07-30 09:37:27 +0300

    Creates grid system

--- src/views/PrincipalDashboard.vue
+++ src/views/PrincipalDashboard.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
+    <div style="height: 100vh;">
         <Navigation/>
+        <v-container fill-height >
+          <v-layout wrap align-center>
+            <v-flex xs4>
+              <v-card dark color="primary">
+                <v-card-text class="px-2">School of engineering card</v-card-text>
+              </v-card>
+            </v-flex>
+
+            <v-flex xs4 px-2>
+              <v-card dark color="primary">
+                <v-card-text class="px-2">School of built environment card </v-card-text>
+              </v-card>
+            </v-flex>
+
+            <v-flex xs4>
+              <v-card dark color="primary">
+                <v-card-text class="px-2"> School of fine art card</v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
     </div>
 </template>
 <script>
