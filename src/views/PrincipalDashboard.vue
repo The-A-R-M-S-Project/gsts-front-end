6c17915991d415207586f5385afdb3754a7cb641
Ben-Cliff <benclifg@gmail.com>
2019-08-24 16:00:47 +0300

    Creates school cards

--- src/views/PrincipalDashboard.vue
+++ src/views/PrincipalDashboard.vue
@@ -1,38 +1,55 @@
 <template>
-    <div style="height: 100vh;">
-        <Navigation/>
-        <v-container fill-height >
-          <v-layout wrap align-center>
-            <v-flex xs4>
-              <v-card dark color="primary" to="/ECE-dashboard">
-                <v-card-text class="px-2">School of engineering card</v-card-text>
-              </v-card>
-            </v-flex>
+  <div style="height: 100vh;">
+    <Navigation />
+    <v-container fill-height>
+      <v-layout wrap align-center>
+        <v-flex xs4>
+          <v-card class="mx-auto" max-width="344">
+            <v-img :src="require('@/assets/principal/ece.jpg')" height="200px"></v-img>
+            <v-card-title>
+              School of engineering
+            </v-card-title>
+            <v-card-actions>
+              <v-btn class="white--text" color="teal" to="/ECE-dashboard">dashboard</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
 
-            <v-flex xs4 px-2>
-              <v-card dark color="primary" >
-                <v-card-text class="px-2">School of built environment card </v-card-text>
-              </v-card>
-            </v-flex>
+        <v-flex xs4 px-3>
+          <v-card class="mx-auto" max-width="344">
+            <v-img :src="require('@/assets/principal/BE.jpg')" height="200px"></v-img>
+            <v-card-title>
+              School of built environment
+            </v-card-title>
+            <v-card-actions>
+              <v-btn class="white--text" color="purple" to="/BE-dashboard">dashboard</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
 
-            <v-flex xs4>
-              <v-card dark color="primary">
-                <v-card-text class="px-2"> School of fine art card</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-    </div>
+        <v-flex xs4>
+          <v-card class="mx-auto" max-width="344">
+            <v-img :src="require('@/assets/principal/FA.jpg')" height="200px"></v-img>
+            <v-card-title>
+              School of fine art
+            </v-card-title>
+            <v-card-actions>
+              <v-btn class="white--text" color="teal" to="/FA-dashboard">dashboard</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 <script>
-import Navigation from "@/components/StudentsNav.vue"
+import Navigation from "@/components/StudentsNav.vue";
 export default {
-    name: 'principal-dashboard',
-    components: {
-      Navigation,
-    }
-}
+  name: "principal-dashboard",
+  components: {
+    Navigation
+  }
+};
 </script>
 <style>
-
 </style>
