4795ce7436d2038ce57b8099ad140fdaba3a989f
Ben-Cliff <benclifg@gmail.com>
2019-08-24 15:10:38 +0300

    Adds ECE path on principal dashboard

--- src/views/PrincipalDashboard.vue
+++ src/views/PrincipalDashboard.vue
@@ -4,13 +4,13 @@
         <v-container fill-height >
           <v-layout wrap align-center>
             <v-flex xs4>
-              <v-card dark color="primary">
+              <v-card dark color="primary" to="/ECE-dashboard">
                 <v-card-text class="px-2">School of engineering card</v-card-text>
               </v-card>
             </v-flex>
 
             <v-flex xs4 px-2>
-              <v-card dark color="primary">
+              <v-card dark color="primary" >
                 <v-card-text class="px-2">School of built environment card </v-card-text>
               </v-card>
             </v-flex>
