0805a2c1024afade9caf3bad3d7f5d2357534be7
rodrickcalvin <rodrickwamala@gmail.com>
2020-01-22 12:31:36 +0300

    Refactors student timeline code

--- src/components/StudentTimeline.vue
+++ src/components/StudentTimeline.vue
@@ -1,10 +1,11 @@
 <template>
   <v-card>
-    <v-timeline align-top dense>
-      <v-timeline-item color="teal" small>
+    <div v-for="x in Events" :key="x._id">
+      <v-timeline align-top dense>
+        <v-timeline-item :color="x.eventState == 'done' ? 'teal': 'grey'" small>
         <v-layout pt-3>
           <v-flex xs3>
-            <strong>Report handed in</strong>
+            <strong>{{x.eventName}}</strong>
           </v-flex>
           <v-flex>
             <strong></strong>
@@ -12,67 +13,18 @@
           </v-flex>
         </v-layout>
       </v-timeline-item>
-
-      <v-timeline-item color="grey" small>
-        <v-layout wrap pt-3>
-          <v-flex xs3>
-            <strong>Report sent to examiner</strong>
-          </v-flex>
-          <v-flex>
-            <strong></strong>
-            <div class="caption mb-2"></div>
-          </v-flex>
-        </v-layout>
-      </v-timeline-item>
-
-      <v-timeline-item color="grey" small>
-        <v-layout pt-3>
-          <v-flex xs3>
-            <strong>Report recieved by examiner</strong>
-          </v-flex>
-          <v-flex>
-            <strong></strong>
-          </v-flex>
-        </v-layout>
-      </v-timeline-item>
-
-      <v-timeline-item color="grey" small>
-        <v-layout pt-3>
-          <v-flex xs3>
-            <strong>Report returned by examiner</strong>
-          </v-flex>
-          <v-flex>
-            <strong></strong>
-            <div class="caption"></div>
-          </v-flex>
-        </v-layout>
-      </v-timeline-item>
-      <v-timeline-item color="grey" small>
-        <v-layout pt-3>
-          <v-flex xs3>
-            <strong>Viva date set</strong>
-          </v-flex>
-          <v-flex>
-            <strong></strong>
-            <div class="caption"></div>
-          </v-flex>
-        </v-layout>
-      </v-timeline-item>
-      <v-timeline-item color="grey" small>
-        <v-layout pt-3>
-          <v-flex xs3>
-            <strong>Vivas done</strong>
-          </v-flex>
-          <v-flex>
-            <strong></strong>
-            <div class="caption"></div>
-          </v-flex>
-        </v-layout>
-      </v-timeline-item>
-    </v-timeline>
+      </v-timeline>
+    </div>
   </v-card>
 </template>
 
 <script>
-export default {};
+import Events from "@/services/events.js";
+export default {
+  data() {
+    return {
+      Events
+    }
+  },
+};
 </script>
