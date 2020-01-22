fd248f45469a2e846ffd04279daaa529679c4121
rodrickcalvin <rodrickwamala@gmail.com>
2020-01-22 12:30:57 +0300

    Creates an events state service file

---
+++ src/services/events.js
@@ -0,0 +1,33 @@
+let eventlist = [
+    {
+        _id: 0,
+        eventName: "Report Handed in",
+        eventState: "done"
+    },
+    {
+        _id: 1,
+        eventName: "Report sent to Examiner",
+        eventState: "not sent"
+    },
+    {
+        _id: 2,
+        eventName: "Report received by examiner",
+        eventState: "not sent"
+    },
+    {
+        _id: 3,
+        eventName: "Report returned by examiner",
+        eventState: "not sent"
+    },
+    {
+        _id: 4,
+        eventName: "Viva date set",
+        eventState: "not sent"
+    },
+    {
+        _id: 5,
+        eventName: "Vivas done",
+        eventState: "not sent"
+    }
+];
+export default eventlist;
