845fb5bf7984af20f7caa4df84861c6571378add
pip4pap <phillip.musumba.52@gmail.com>
2020-03-09 08:30:02 +0300

    Adds a NODE(express) server to deploy application

---
+++ server.js
@@ -0,0 +1,7 @@
+let express = require("express");
+let serveStatic = require("serve-static");
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+let port = process.env.PORT || 5000;
+app.listen(port);
+console.log("Server started " + port);
