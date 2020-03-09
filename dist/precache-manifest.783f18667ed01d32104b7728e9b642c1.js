831c97ae7979c5680eb5bc3684f6c479d726839a
pip4pap <phillip.musumba.52@gmail.com>
2020-03-09 08:39:23 +0300

    Adds a Heroku Procfile to specify the process to start when serving the app

---
+++ dist/precache-manifest.783f18667ed01d32104b7728e9b642c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "696f03c97e914c7263d5cc903a0df3c2",
+    "url": "/img/FA.696f03c9.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a3c03dce45cd7a910afe",
+    "url": "/js/chunk-vendors.704b91e9.js"
+  },
+  {
+    "revision": "4c86027eb4977e8d5dc6",
+    "url": "/js/app.b94915a2.js"
+  },
+  {
+    "revision": "bcd57d02ce912449cd49573231f6cba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a660310ef41ea192246d428f32350b2c",
+    "url": "/img/login-2.a660310e.jpg"
+  },
+  {
+    "revision": "6ed0c16ee9e9a4debcf68d447a863699",
+    "url": "/img/login-1.6ed0c16e.jpg"
+  },
+  {
+    "revision": "a1b586f9fcacc1102dfe6ff3ed1c98e8",
+    "url": "/img/ece.a1b586f9.jpg"
+  },
+  {
+    "revision": "af837fcbdc3fa925fc94d97e3bf95f60",
+    "url": "/img/BE.af837fcb.jpg"
+  },
+  {
+    "revision": "a3c03dce45cd7a910afe",
+    "url": "/css/chunk-vendors.059bd780.css"
+  },
+  {
+    "revision": "4c86027eb4977e8d5dc6",
+    "url": "/css/app.75e4f292.css"
+  }
+];
