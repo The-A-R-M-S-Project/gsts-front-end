12a34251f4509325e2e604fd13a9de8cc462fa17
pip4pap <phillip.musumba.52@gmail.com>
2020-03-13 11:17:31 +0300

    Logins in the user

--- src/ store/modules/authentication.js
+++ src/ store/modules/authentication.js
@@ -1,4 +1,53 @@
-const state = {};
-const mutations = {};
-const actions = {};
-const getters = {};
+import axiosInstance from "../axios_setup";
+
+const state = {
+    isLoading: false,
+    loginError: null,
+    isLoggedIn: false,
+    user: {}
+};
+const mutations = {
+    IsLoading(state, payload) {
+        state.isLoading = payload;
+    },
+    LoginError(state, error) {
+        state.loginError = error;
+    },
+    SetUser(state, payload) {
+        state.user = payload;
+        state.isLoggedIn = true;
+    }
+};
+const actions = {
+    async login({ commit }, data) {
+        await axiosInstance
+            .post("/staff/login", data)
+            .then(response => {
+                commit("IsLoading", true);
+                localStorage.setItem(
+                    "user",
+                    JSON.stringify(response.data.data.user)
+                );
+                localStorage.setItem("jwt", response.data.token);
+                commit("SetUser", response.data.data.user);
+                commit("IsLoading", false);
+            })
+            .catch(error => {
+                commit("IsLoading", false);
+                commit("LoginError", error.response.data.message);
+            });
+    }
+};
+const getters = {
+    isLoggedIn: state => state.isLoggedIn,
+    user: state => state.user,
+    isLoading: state => state.isLoading,
+    loginError: state => state.loginError
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+};
--- src/components/StaffLoginForm.vue
+++ src/components/StaffLoginForm.vue
@@ -1,42 +1,43 @@
 <template>
-<div>
-  <v-form ref="form" v-model="form" class="login-form" name="login">
-    <v-container>
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="College Email"
-        prepend-inner-icon="person"
-        type="email"
-        required
-        color="purple"
-      ></v-text-field>
-      <v-text-field
-        v-model="password"
-        label="password"
-        prepend-inner-icon="lock"
-        type="password"
-        required
-        color="purple"
-      ></v-text-field>
-      <div class="px-5">
-        <v-btn
-          round
-          large
-          block
-          depressed
-          ripple
-          class="yellow font-weight-bold"
-          type="submit"
-          v-on:click="submitForm"
-        >
-          <v-icon>subdirectory_arrow_right</v-icon>
-          <span>&nbsp;Login</span>
-        </v-btn>
-      </div>
-    </v-container>
-  </v-form>
-  <Alert :show="message" />
+  <div>
+    <v-form ref="form" v-model="form" class="login-form" name="login">
+      <v-container>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="College Email"
+          prepend-inner-icon="person"
+          type="email"
+          required
+          color="purple"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="password"
+          prepend-inner-icon="lock"
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="show ? 'text' : 'password'"
+          required
+          color="purple"
+        ></v-text-field>
+        <div class="px-5">
+          <v-btn
+            round
+            large
+            block
+            depressed
+            ripple
+            class="yellow font-weight-bold"
+            type="submit"
+            @click="login"
+          >
+            <v-icon>subdirectory_arrow_right</v-icon>
+            <span>&nbsp;Login</span>
+          </v-btn>
+        </div>
+      </v-container>
+    </v-form>
+    <Alert :show="message" />
   </div>
 </template>
 
@@ -51,6 +52,7 @@ export default {
   data: () => ({
     form: false,
     message: false,
+    show: false,
     email: "",
     emailRules: [
       emailField =>
@@ -65,42 +67,46 @@ export default {
     required: [field => !!field || "This field is required"]
   }),
   methods: {
-    submitForm(event) {
+    login() {
       event.preventDefault();
       if (!this.password == "" && !this.email == "") {
-        this.$http
-          .post(
-            "https://arms-graduate-student-tracker.herokuapp.com/api/staff/login",
-            {
-              email: this.email,
-              password: this.password
-            }
-          )
-          .then(response => {
-            console.log({ response });
-            localStorage.setItem(
-              "user",
-              JSON.stringify(response.data.data.user)
-            );
-            localStorage.setItem("jwt", response.data.token);
-
-            if (localStorage.getItem("jwt") != null) {
-              this.$emit("loggedIn");
+        this.$store
+          .dispatch("login", {
+            email: this.email,
+            password: this.password
+          })
+          .then(() => {
+            if (this.isLogged) {
               if (this.$route.params.continue != null) {
                 this.$router.push(this.$route.params.continue);
               } else {
-                const user = response.data.data.user;
+                const user = this.user;
                 this.$router.push({ name: `${user.role}-dashboard` });
               }
             }
           })
           .catch(error => {
             this.message = true;
-            console.log(error.response.data.message);
+            console.log(error);
+            console.log(this.loginError);
           });
       } else {
-        alert("Please provide both email and password to log in");
+        alert("Please provide both email and password to log in!");
       }
+    }
+  },
+  computed: {
+    isLogged() {
+      return this.$store.getters.isLoggedIn;
+    },
+    user() {
+      return this.$store.getters.user;
+    },
+    isLoading() {
+      return this.$store.getters.isLoading;
+    },
+    loginError() {
+      return this.$store.getters.loginError;
     }
   }
 };
