9201d37075cb39040021296534edf4862cfba911
pip4pap <phillip.musumba.52@gmail.com>
2020-06-10 12:36:03 +0300

    Routes user back to previous page instead of login when they visit a page that doesn't exist

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,37 +1,42 @@
 <template>
-  <div id="page-not-found">
-    <span class="grey--text display-1 emptyPane text-center custom-font-family">
-      <v-icon large color="purple">mdi-power-plug-off</v-icon>&nbsp; That link has no power here!
-      <br />
-      <span class="title custom-font-family">
-        Please sign in
+    <div id="page-not-found">
         <span
-          class="yellow--text text--darken-3"
-          style="cursor: pointer"
-          @click="returnToApp"
-        >here</span>
-      </span>
-    </span>
-  </div>
+            class="grey--text display-1 emptyPane text-center custom-font-family"
+        >
+            <v-icon large color="purple">mdi-power-plug-off</v-icon>&nbsp; That
+            link has no power here!
+            <br />
+            <span class="title custom-font-family">
+                Return to previous page
+                <span
+                    class="yellow--text text--darken-3"
+                    style="cursor: pointer"
+                    @click="returnToApp"
+                >
+                    here</span
+                >
+            </span>
+        </span>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "PageNotFound",
-  methods: {
-    returnToApp() {
-      this.$router.push("/");
-    }
-  }
+    name: "PageNotFound",
+    methods: {
+        returnToApp() {
+            this.$router.go(-1);
+        },
+    },
 };
 </script>
 
 <style>
 .emptyPane {
-  display: inline-block;
-  margin-top: 40vh;
+    display: inline-block;
+    margin-top: 40vh;
 }
 #page-not-found {
-  text-align: center;
+    text-align: center;
 }
 </style>
