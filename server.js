f02668731de64149f2d40110ac1afe535d8af4d2
pip4pap <phillip.musumba.52@gmail.com>
2020-06-29 22:19:54 +0300

    Refactors server code to fallback to index.html for unknown requests

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-let express = require("express");
-let serveStatic = require("serve-static");
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-let port = process.env.PORT || 5000;
-app.listen(port);
-console.log("Server started " + port);
+const express = require("express");
+const app = express();
+const port = 8020;
+const buildLocation = "dist";
+app.use(express.static(`${buildLocation}`));
+app.use((req, res, next) => {
+    if (!req.originalUrl.includes(buildLocation)) {
+        res.sendFile(`${__dirname}/${buildLocation}/index.html`);
+    } else {
+        next();
+    }
+});
+app.listen(port, () => console.info(`Server running on port ${port}`));
