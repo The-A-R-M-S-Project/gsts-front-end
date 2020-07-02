ce0dc58b60fa44cf7dd6f67bb10c7251788b8c08
pip4pap <phillip.musumba.52@gmail.com>
2020-07-02 10:25:07 +0300

    Resizes login error alerts for mobile

--- src/components/StaffLoginForm.vue
+++ src/components/StaffLoginForm.vue
@@ -1,157 +1,167 @@
 <template>
-  <div>
-    <v-alert
-      v-model="displayLoginError"
-      type="error"
-      dismissible
-      class="mx-7 mt-2 error-alert"
-    >{{ loginError }}</v-alert>
-    <v-form ref="staffLoginForm" v-model="valid" class="login-form" name="login" lazy-validation>
-      <v-container>
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          label="College Email"
-          prepend-inner-icon="mdi-account"
-          class="px-7"
-          :class="{'pt-5 sub-heading': $vuetify.breakpoint.xs}"
-          type="email"
-          clearable
-          height="2rem"
-          required
-          color="purple"
-        ></v-text-field>
-        <v-text-field
-          v-model="password"
-          label="password"
-          prepend-inner-icon="mdi-lock"
-          class="px-7"
-          :class="{'pt-5 sub-heading': $vuetify.breakpoint.xs}"
-          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show ? 'text' : 'password'"
-          @click:append="show = !show"
-          required
-          color="purple"
-        ></v-text-field>
-        <p
-          id="forgot-staff-password"
-          class="text-center purple--text"
-          :class="{'pt-1 pb-4 sub-heading': $vuetify.breakpoint.xs}"
-          @click="forgotStaffPassword"
-        >Forgot password?</p>
-        <div class="px-5 text-center">
-          <v-btn
-            rounded
-            large
-            depressed
-            :loading="isLoading"
-            ripple
-            width="400"
-            class="yellow font-weight-bold d-none d-sm-flex"
-            type="submit"
-            @click="login"
-          >
-            <v-icon class>mdi-subdirectory-arrow-right</v-icon>
-            <span>&nbsp;Login</span>
-          </v-btn>
-          <v-btn
-            rounded
-            large
-            depressed
-            :loading="isLoading"
-            height="3.5rem"
-            ripple
-            width="400"
-            class="yellow font-weight-bold button-text d-flex d-sm-none"
-            type="submit"
-            @click="login"
-          >
-            <v-icon large>mdi-subdirectory-arrow-right</v-icon>
-            <span>&nbsp;Login</span>
-          </v-btn>
-        </div>
-      </v-container>
-    </v-form>
-  </div>
+    <div>
+        <v-alert
+            v-model="displayLoginError"
+            type="error"
+            dismissible
+            class="mx-7 mt-2 error-alert"
+            :class="{ 'normal-text': $vuetify.breakpoint.xs }"
+            >{{ loginError }}</v-alert
+        >
+        <v-form
+            ref="staffLoginForm"
+            v-model="valid"
+            class="login-form"
+            name="login"
+            lazy-validation
+        >
+            <v-container>
+                <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="College Email"
+                    prepend-inner-icon="mdi-account"
+                    class="px-7"
+                    :class="{ 'pt-5 sub-heading': $vuetify.breakpoint.xs }"
+                    type="email"
+                    clearable
+                    height="2rem"
+                    required
+                    color="purple"
+                ></v-text-field>
+                <v-text-field
+                    v-model="password"
+                    label="password"
+                    prepend-inner-icon="mdi-lock"
+                    class="px-7"
+                    :class="{ 'pt-5 sub-heading': $vuetify.breakpoint.xs }"
+                    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show ? 'text' : 'password'"
+                    @click:append="show = !show"
+                    required
+                    color="purple"
+                ></v-text-field>
+                <p
+                    id="forgot-staff-password"
+                    class="text-center purple--text"
+                    :class="{ 'pt-1 pb-4 sub-heading': $vuetify.breakpoint.xs }"
+                    @click="forgotStaffPassword"
+                >
+                    Forgot password?
+                </p>
+                <div class="px-5 text-center">
+                    <v-btn
+                        rounded
+                        large
+                        depressed
+                        :loading="isLoading"
+                        ripple
+                        width="400"
+                        class="yellow font-weight-bold d-none d-sm-inline-block"
+                        type="submit"
+                        @click="login"
+                    >
+                        <v-icon class>mdi-subdirectory-arrow-right</v-icon>
+                        <span>&nbsp;Login</span>
+                    </v-btn>
+                    <v-btn
+                        rounded
+                        large
+                        depressed
+                        :loading="isLoading"
+                        height="3.5rem"
+                        ripple
+                        width="400"
+                        class="yellow font-weight-bold button-text d-flex d-sm-none"
+                        type="submit"
+                        @click="login"
+                    >
+                        <v-icon large>mdi-subdirectory-arrow-right</v-icon>
+                        <span>&nbsp;Login</span>
+                    </v-btn>
+                </div>
+            </v-container>
+        </v-form>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "staff-login-form",
-  data: () => ({
-    valid: true,
-    loading: false,
-    show: false,
-    displayLoginError: false,
-    email: "",
-    emailRules: [
-      emailField =>
-        /.+@+/.test(emailField) || "Please enter a valid college email"
-    ],
-    password: "",
-    passwordRules: len => [
-      passwordField =>
-        (passwordField || "").length >= len ||
-        `Invalid character length, required ${len}`,
-      password => !!password || "Password is required"
-    ],
-    required: [field => !!field || "This field is required"]
-  }),
-  computed: {
-    isLogged() {
-      return this.$store.getters.isLoggedIn;
+    name: "staff-login-form",
+    data: () => ({
+        valid: true,
+        loading: false,
+        show: false,
+        displayLoginError: false,
+        email: "",
+        emailRules: [
+            (emailField) =>
+                /.+@+/.test(emailField) || "Please enter a valid college email",
+        ],
+        password: "",
+        passwordRules: (len) => [
+            (passwordField) =>
+                (passwordField || "").length >= len ||
+                `Invalid character length, required ${len}`,
+            (password) => !!password || "Password is required",
+        ],
+        required: [(field) => !!field || "This field is required"],
+    }),
+    computed: {
+        isLogged() {
+            return this.$store.getters.isLoggedIn;
+        },
+        user() {
+            return this.$store.getters.user;
+        },
+        isLoading() {
+            return this.$store.getters.isLoading;
+        },
+        loginError() {
+            return this.$store.getters.loginError;
+        },
     },
-    user() {
-      return this.$store.getters.user;
-    },
-    isLoading() {
-      return this.$store.getters.isLoading;
-    },
-    loginError() {
-      return this.$store.getters.loginError;
-    }
-  },
-  methods: {
-    login() {
-      event.preventDefault();
-      if (this.$refs.staffLoginForm.validate()) {
-        this.$store
-          .dispatch("login", {
-            user: "staff",
-            credentials: {
-              email: this.email,
-              password: this.password
+    methods: {
+        login() {
+            event.preventDefault();
+            if (this.$refs.staffLoginForm.validate()) {
+                this.$store
+                    .dispatch("login", {
+                        user: "staff",
+                        credentials: {
+                            email: this.email,
+                            password: this.password,
+                        },
+                    })
+                    .then(() => {
+                        if (this.isLogged) {
+                            if (this.$route.params.continue != null) {
+                                this.$router.push(this.$route.params.continue);
+                            } else {
+                                const user = this.user;
+                                this.$router.push({
+                                    name: `${user.role}-dashboard`,
+                                });
+                            }
+                        } else {
+                            this.displayLoginError = true;
+                        }
+                    });
             }
-          })
-          .then(() => {
-            if (this.isLogged) {
-              if (this.$route.params.continue != null) {
-                this.$router.push(this.$route.params.continue);
-              } else {
-                const user = this.user;
-                this.$router.push({
-                  name: `${user.role}-dashboard`
-                });
-              }
-            } else {
-              this.displayLoginError = true;
-            }
-          });
-      }
+        },
+        forgotStaffPassword() {
+            this.$router.push("/forgot-staff-password");
+        },
     },
-    forgotStaffPassword() {
-      this.$router.push("/forgot-staff-password");
-    }
-  }
 };
 </script>
 
 <style>
 .error-alert {
-  border-radius: 2rem !important;
-  height: 3.4rem;
+    border-radius: 2rem !important;
+    height: 3.4rem;
 }
 #forgot-staff-password {
-  cursor: pointer;
+    cursor: pointer;
 }
 </style>
