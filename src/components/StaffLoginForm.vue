0c79d0783885ec31c699b22f40bf956ef0e3654d
pip4pap <phillip.musumba.52@gmail.com>
2020-07-06 15:23:31 +0300

    Refactors resizing of login screen to avoid zoom out effect on mobile

--- src/components/StaffLoginForm.vue
+++ src/components/StaffLoginForm.vue
@@ -1,172 +1,164 @@
 <template>
-    <div>
-        <v-alert
-            v-model="displayLoginError"
-            type="error"
-            dismissible
-            class="mx-7 mt-2 error-alert"
-            :class="{ 'normal-text': $vuetify.breakpoint.xs }"
-            >{{ loginError }}</v-alert
-        >
-        <v-form
-            ref="staffLoginForm"
-            v-model="valid"
-            name="login"
-            lazy-validation
-        >
-            <v-container>
-                <v-text-field
-                    v-model="email"
-                    :rules="emailRules"
-                    label="College Email"
-                    prepend-inner-icon="mdi-account"
-                    :class="{
+  <div>
+    <v-alert
+      v-model="displayLoginError"
+      type="error"
+      dismissible
+      class="mx-7 mt-2 error-alert"
+      :class="{ 'normal-text': $vuetify.breakpoint.xs }"
+    >{{ loginError }}</v-alert>
+    <v-form ref="staffLoginForm" v-model="valid" name="login" lazy-validation>
+      <v-container>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="College Email"
+          prepend-inner-icon="mdi-account"
+          :class="{
                         'pt-5 px-2 styled-input sub-heading':
                             $vuetify.breakpoint.xs,
                         'px-7': !$vuetify.breakpoint.xs,
                     }"
-                    type="email"
-                    clearable
-                    height="2rem"
-                    required
-                    color="purple"
-                ></v-text-field>
-                <v-text-field
-                    v-model="password"
-                    label="Password"
-                    prepend-inner-icon="mdi-lock"
-                    :class="{
+          type="email"
+          clearable
+          height="2rem"
+          required
+          color="purple"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="Password"
+          prepend-inner-icon="mdi-lock"
+          :class="{
                         'pt-5 px-2 styled-input sub-heading':
                             $vuetify.breakpoint.xs,
                         'px-7': !$vuetify.breakpoint.xs,
                     }"
-                    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                    :type="show ? 'text' : 'password'"
-                    @click:append="show = !show"
-                    required
-                    color="purple"
-                ></v-text-field>
-                <p
-                    id="forgot-staff-password"
-                    class="text-center purple--text"
-                    :class="{ 'pt-1 pb-4 sub-heading': $vuetify.breakpoint.xs }"
-                    @click="forgotStaffPassword"
-                >
-                    Forgot password?
-                </p>
-                <div class="px-5 text-center">
-                    <v-btn
-                        rounded
-                        large
-                        depressed
-                        :loading="isLoading"
-                        ripple
-                        width="400"
-                        class="yellow font-weight-bold d-none d-sm-inline-block"
-                        type="submit"
-                        @click="login"
-                    >
-                        <v-icon class>mdi-subdirectory-arrow-right</v-icon>
-                        <span>&nbsp;Login</span>
-                    </v-btn>
-                    <v-btn
-                        rounded
-                        large
-                        depressed
-                        :loading="isLoading"
-                        height="3.5rem"
-                        ripple
-                        width="400"
-                        class="yellow font-weight-bold mb-4 button-text d-flex d-sm-none"
-                        type="submit"
-                        @click="login"
-                    >
-                        <v-icon large>mdi-subdirectory-arrow-right</v-icon>
-                        <span>&nbsp;Login</span>
-                    </v-btn>
-                </div>
-            </v-container>
-        </v-form>
-    </div>
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="show ? 'text' : 'password'"
+          @click:append="show = !show"
+          required
+          color="purple"
+        ></v-text-field>
+        <p
+          id="forgot-staff-password"
+          class="text-center purple--text"
+          :class="{ 'pt-1 pb-4 sub-heading': $vuetify.breakpoint.xs }"
+          @click="forgotStaffPassword"
+        >Forgot password?</p>
+        <div class="px-5 text-center">
+          <v-btn
+            rounded
+            large
+            depressed
+            :loading="isLoading"
+            ripple
+            width="400"
+            class="yellow font-weight-bold d-none d-sm-inline-block"
+            type="submit"
+            @click="login"
+          >
+            <v-icon class>mdi-subdirectory-arrow-right</v-icon>
+            <span>&nbsp;Login</span>
+          </v-btn>
+          <v-btn
+            rounded
+            large
+            depressed
+            width="100%"
+            :loading="isLoading"
+            height="3.5rem"
+            ripple
+            class="yellow font-weight-bold mb-4 button-text d-flex d-sm-none"
+            type="submit"
+            @click="login"
+          >
+            <v-icon large>mdi-subdirectory-arrow-right</v-icon>
+            <span>&nbsp;Login</span>
+          </v-btn>
+        </div>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "staff-login-form",
-    data: () => ({
-        valid: true,
-        loading: false,
-        show: false,
-        displayLoginError: false,
-        email: "",
-        emailRules: [
-            (emailField) =>
-                /.+@+/.test(emailField) || "Please enter a valid college email",
-        ],
-        password: "",
-        passwordRules: (len) => [
-            (passwordField) =>
-                (passwordField || "").length >= len ||
-                `Invalid character length, required ${len}`,
-            (password) => !!password || "Password is required",
-        ],
-        required: [(field) => !!field || "This field is required"],
-    }),
-    computed: {
-        isLogged() {
-            return this.$store.getters.isLoggedIn;
-        },
-        user() {
-            return this.$store.getters.user;
-        },
-        isLoading() {
-            return this.$store.getters.isLoading;
-        },
-        loginError() {
-            return this.$store.getters.loginError;
-        },
+  name: "staff-login-form",
+  data: () => ({
+    valid: true,
+    loading: false,
+    show: false,
+    displayLoginError: false,
+    email: "",
+    emailRules: [
+      emailField =>
+        /.+@+/.test(emailField) || "Please enter a valid college email"
+    ],
+    password: "",
+    passwordRules: len => [
+      passwordField =>
+        (passwordField || "").length >= len ||
+        `Invalid character length, required ${len}`,
+      password => !!password || "Password is required"
+    ],
+    required: [field => !!field || "This field is required"]
+  }),
+  computed: {
+    isLogged() {
+      return this.$store.getters.isLoggedIn;
     },
-    methods: {
-        login() {
-            event.preventDefault();
-            if (this.$refs.staffLoginForm.validate()) {
-                this.$store
-                    .dispatch("login", {
-                        user: "staff",
-                        credentials: {
-                            email: this.email,
-                            password: this.password,
-                        },
-                    })
-                    .then(() => {
-                        if (this.isLogged) {
-                            if (this.$route.params.continue != null) {
-                                this.$router.push(this.$route.params.continue);
-                            } else {
-                                const user = this.user;
-                                this.$router.push({
-                                    name: `${user.role}-dashboard`,
-                                });
-                            }
-                        } else {
-                            this.displayLoginError = true;
-                        }
-                    });
+    user() {
+      return this.$store.getters.user;
+    },
+    isLoading() {
+      return this.$store.getters.isLoading;
+    },
+    loginError() {
+      return this.$store.getters.loginError;
+    }
+  },
+  methods: {
+    login() {
+      event.preventDefault();
+      if (this.$refs.staffLoginForm.validate()) {
+        this.$store
+          .dispatch("login", {
+            user: "staff",
+            credentials: {
+              email: this.email,
+              password: this.password
             }
-        },
-        forgotStaffPassword() {
-            this.$router.push("/forgot-staff-password");
-        },
+          })
+          .then(() => {
+            if (this.isLogged) {
+              if (this.$route.params.continue != null) {
+                this.$router.push(this.$route.params.continue);
+              } else {
+                const user = this.user;
+                this.$router.push({
+                  name: `${user.role}-dashboard`
+                });
+              }
+            } else {
+              this.displayLoginError = true;
+            }
+          });
+      }
     },
+    forgotStaffPassword() {
+      this.$router.push("/forgot-staff-password");
+    }
+  }
 };
 </script>
 
 <style>
 .error-alert {
-    border-radius: 2rem !important;
-    height: 3.4rem;
+  border-radius: 2rem !important;
+  height: 3.4rem;
 }
 #forgot-staff-password {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,100 +1,74 @@
 <template>
-    <div class="login-page" :class="{ 'auth-pages': !$vuetify.breakpoint.xs }">
-        <v-container fluid text-xs-center class="login d-none d-sm-flex">
-            <v-row align="center" justify="center">
-                <v-card max-width="90%" class="elevation-24">
-                    <v-row no-gutters>
-                        <v-col md="7" class="pa-3">
-                            <v-card-text>
-                                <v-container>
-                                    <h2
-                                        class="headline pl-md-5 mb-8 custom-font-family text-center"
-                                    >
-                                        Login
-                                    </h2>
-                                    <v-tabs v-model="tabs" centered>
-                                        <v-tabs-slider
-                                            color="purple"
-                                        ></v-tabs-slider>
+  <div :class="{ 'auth-pages': !$vuetify.breakpoint.xs }">
+    <v-container fluid text-xs-center class="login d-none d-sm-flex">
+      <v-row align="center" justify="center">
+        <v-card max-width="90%" class="elevation-24">
+          <v-row no-gutters>
+            <v-col md="7" class="pa-3">
+              <v-card-text>
+                <v-container>
+                  <h2 class="headline pl-md-5 mb-8 custom-font-family text-center">Login</h2>
+                  <v-tabs v-model="tabs" centered>
+                    <v-tabs-slider color="purple"></v-tabs-slider>
 
-                                        <v-tab
-                                            href="#staff"
-                                            class="text-capitalize black--text py-4"
-                                        >
-                                            <span class="tab-headings px-7"
-                                                >Staff</span
-                                            >
-                                        </v-tab>
+                    <v-tab href="#staff" class="text-capitalize black--text py-4">
+                      <span class="tab-headings px-7">Staff</span>
+                    </v-tab>
 
-                                        <v-tab
-                                            href="#student"
-                                            class="text-capitalize black--text py-4"
-                                        >
-                                            <span class="tab-headings px-7"
-                                                >Student</span
-                                            >
-                                        </v-tab>
-                                    </v-tabs>
+                    <v-tab href="#student" class="text-capitalize black--text py-4">
+                      <span class="tab-headings px-7">Student</span>
+                    </v-tab>
+                  </v-tabs>
 
-                                    <v-tabs-items v-model="tabs">
-                                        <v-tab-item :value="'staff'">
-                                            <StaffLoginForm />
-                                        </v-tab-item>
-                                        <v-tab-item :value="'student'">
-                                            <StudentLoginForm />
-                                        </v-tab-item>
-                                    </v-tabs-items>
-                                </v-container>
-                            </v-card-text>
-                        </v-col>
-                        <v-col md="5">
-                            <RegisterText />
-                        </v-col>
-                    </v-row>
-                </v-card>
-            </v-row>
-        </v-container>
-        <div class="d-flex d-sm-none">
-            <v-row no-gutters>
-                <v-col class="register-text" md="5">
-                    <RegisterText />
-                </v-col>
-                <v-col md="7" class="pa-3">
-                    <h2
-                        class="heading pl-md-5 mb-8 mt-8 custom-font-family text-center"
-                    >
-                        Login
-                    </h2>
-                    <v-tabs v-model="tabs" centered>
-                        <v-tabs-slider color="purple"></v-tabs-slider>
+                  <v-tabs-items v-model="tabs">
+                    <v-tab-item :value="'staff'">
+                      <StaffLoginForm />
+                    </v-tab-item>
+                    <v-tab-item :value="'student'">
+                      <StudentLoginForm />
+                    </v-tab-item>
+                  </v-tabs-items>
+                </v-container>
+              </v-card-text>
+            </v-col>
+            <v-col md="5">
+              <RegisterText />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-row>
+    </v-container>
+    <div class="d-block d-sm-none">
+      <v-row no-gutters>
+        <v-col class="register-text" md="5" xs="12">
+          <RegisterText />
+        </v-col>
+        <v-col md="7" xs="12" class="pa-3">
+          <h2 class="heading pl-md-5 mb-8 mt-8 custom-font-family text-center">Login</h2>
+          <v-tabs v-model="tabs" centered>
+            <v-tabs-slider color="purple"></v-tabs-slider>
 
-                        <v-tab
-                            href="#staff"
-                            class="text-capitalize black--text py-4"
-                        >
-                            <span class="sub-heading px-7">Staff</span>
-                        </v-tab>
+            <v-tab href="#staff" class="text-capitalize black--text py-4">
+              <span class="sub-heading px-3">Staff</span>
+            </v-tab>
 
-                        <v-tab
-                            href="#student"
-                            class="text-capitalize black--text py-4"
-                        >
-                            <span class="sub-heading px-7">Student</span>
-                        </v-tab>
-                    </v-tabs>
+            <v-tab href="#student" class="text-capitalize black--text py-4">
+              <span class="sub-heading px-3">Student</span>
+            </v-tab>
+          </v-tabs>
 
-                    <v-tabs-items v-model="tabs">
-                        <v-tab-item :value="'staff'">
-                            <StaffLoginForm />
-                        </v-tab-item>
-                        <v-tab-item :value="'student'">
-                            <StudentLoginForm />
-                        </v-tab-item>
-                    </v-tabs-items>
-                </v-col>
-            </v-row>
-        </div>
+          <v-tabs-items v-model="tabs">
+            <v-tab-item :value="'staff'">
+              <StaffLoginForm />
+            </v-tab-item>
+            <v-tab-item :value="'student'">
+              <StudentLoginForm />
+            </v-tab-item>
+          </v-tabs-items>
+        </v-col>
+      </v-row>
     </div>
+  </div>
 </template>
 
 <script>
@@ -102,63 +76,41 @@ import StaffLoginForm from "@/components/StaffLoginForm";
 import StudentLoginForm from "@/components/StudentLoginForm";
 import RegisterText from "@/components/RegisterText";
 export default {
-    components: {
-        StaffLoginForm,
-        StudentLoginForm,
-        RegisterText,
-    },
-    data() {
-        return {
-            tabs: null,
-        };
-    },
+  components: {
+    StaffLoginForm,
+    StudentLoginForm,
+    RegisterText
+  },
+  data() {
+    return {
+      tabs: null
+    };
+  }
 };
 </script>
 <style lang="scss">
-.overlayed {
-    width: 100%;
-    height: 100%;
-    background-size: cover;
-    background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
-        url("~@/assets/img/login-2.jpg");
-    position: relative;
-    color: white;
-    background-position: center;
-}
 .tab-headings {
-    font-size: 1.25rem;
-}
-.overlayed p {
-    font-size: 18px;
-    letter-spacing: 150%;
-}
-.overlayed h1 {
-    font-size: 25px;
-    font-weight: lighter;
+  font-size: 1.25rem;
 }
 .auth-pages {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    height: 100%;
-    width: 100%;
-    padding: 12px;
-    margin-right: auto;
-    margin-left: auto;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  height: 100%;
+  width: 100%;
+  padding: 12px;
+  margin-right: auto;
+  margin-left: auto;
 }
 .register-text {
-    height: 63%;
-}
-.login-page {
-    height: 100%;
-    width: 100%;
+  height: 100vh;
 }
 .login {
-    &__register-btn {
-        opacity: 0.65;
-        width: 80%;
-        margin: auto;
-        font-weight: bold;
-    }
+  &__register-btn {
+    opacity: 0.65;
+    width: 80%;
+    margin: auto;
+    font-weight: bold;
+  }
 }
 </style>
