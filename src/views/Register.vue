1ff3347941ba8f2f2b9634be5d56b4f6051aa820
pip4pap <phillip.musumba.52@gmail.com>
2020-07-07 15:57:14 +0300

    Resizes sign up page components for mobile

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,127 +1,262 @@
 <template>
-  <v-container class="register" fluid fill-height>
-    <v-row align="center" justify="center">
-      <v-card max-width="90%" class="elevation-24">
-        <v-row no-gutters>
-          <v-col md="5">
-            <v-container class="text-center" fill-height overlayed>
-              <v-row>
-                <v-col class="px-4 py-2">
-                  <h1 class="display-3 font-weight-light">Welcome</h1>
-                  <p class="mt-5 mb-4">
-                    To keep track of your report please
-                    login with your personal information
-                  </p>
-                  <v-btn rounded large depressed to="/" class="register__login-btn grey">
-                    <v-icon>mdi-subdirectory-arrow-right</v-icon>
-                    <span>&nbsp;Login</span>
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-col>
-          <v-col md="7">
-            <v-container>
-              <v-form ref="registerForm" v-model="valid">
-                <h2 class="headline py-2 text-center">Register</h2>
-                <v-alert
-                  v-model="displaySignUpError"
-                  type="error"
-                  dismissible
-                  class="mx-7 mt-2 error-alert"
-                >{{ signupError }}</v-alert>
-                <v-container class="container px-8 text-center">
-                  <v-row>
-                    <v-col>
-                      <v-text-field
-                        v-model="firstName"
-                        label="First name"
-                        :rules="nameRules"
-                        prepend-inner-icon="mdi-account"
-                        height="28"
-                        color="purple"
-                        required
-                      ></v-text-field>
-                    </v-col>
-
-                    <v-col>
-                      <v-text-field
-                        v-model="lastName"
-                        label="Last name"
-                        :rules="nameRules"
-                        prepend-inner-icon="mdi-account"
-                        height="28"
-                        color="purple"
-                        required
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                  <v-text-field
-                    v-model="email"
-                    label="Email"
-                    :rules="emailRules"
-                    required
-                    prepend-inner-icon="mdi-account"
-                    height="28"
-                    color="purple"
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="phoneNumber"
-                    label="Phone Number e.g +2567... or 07..."
-                    :rules="phoneNumberRules"
-                    required
-                    prepend-inner-icon="mdi-phone"
-                    height="28"
-                    color="purple"
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="password"
-                    :rules="passwordRules(8)"
-                    label="Password"
-                    required
-                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                    :type="showPassword ? 'text' : 'password'"
-                    @click:append="showPassword = !showPassword"
-                    prepend-inner-icon="mdi-lock"
-                    height="28"
-                    color="purple"
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="passwordConfirm"
-                    label="Confirm Password"
-                    :rules="passwordConfirmRules"
-                    required
-                    :append-icon="showConfirm ? 'mdi-eye' : 'mdi-eye-off'"
-                    :type="showConfirm ? 'text' : 'password'"
-                    @click:append="showConfirm = !showConfirm"
-                    prepend-inner-icon="mdi-lock"
-                    height="28"
-                    color="purple"
-                  ></v-text-field>
-                  <div class="mx-5">
-                    <v-btn
-                      type="submit"
-                      large
-                      width="400"
-                      rounded
-                      :loading="isLoading"
-                      depressed
-                      ripple
-                      class="yellow font-weight-bold"
-                      @click="register"
-                    >
-                      <v-icon>mdi-account-plus</v-icon>
-                      <span>&nbsp;Register</span>
+  <div :class="{ 'auth-pages': !$vuetify.breakpoint.xs }">
+    <v-container class="register d-none d-sm-flex" fluid fill-height>
+      <v-row align="center" justify="center">
+        <v-card max-width="90%" class="elevation-24">
+          <v-row no-gutters>
+            <v-col md="5">
+              <v-container class="text-center" fill-height overlayed>
+                <v-row>
+                  <v-col class="px-4 py-2">
+                    <h1 class="display-3 font-weight-light">Welcome</h1>
+                    <p class="mt-5 mb-4">
+                      To keep track of your report please
+                      login with your personal information
+                    </p>
+                    <v-btn rounded large depressed to="/" class="register__login-btn grey">
+                      <v-icon>mdi-subdirectory-arrow-right</v-icon>
+                      <span>&nbsp;Login</span>
                     </v-btn>
-                  </div>
-                </v-container>
-              </v-form>
-            </v-container>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-col>
+            <v-col md="7">
+              <v-container>
+                <v-form ref="registerForm" v-model="valid">
+                  <h2 class="headline py-2 text-center">Register</h2>
+                  <v-alert
+                    v-model="displaySignUpError"
+                    type="error"
+                    dismissible
+                    class="mx-7 mt-2 error-alert"
+                  >{{ signupError }}</v-alert>
+                  <v-container class="container px-8 text-center">
+                    <v-row>
+                      <v-col>
+                        <v-text-field
+                          v-model="firstName"
+                          label="First name"
+                          :rules="nameRules"
+                          prepend-inner-icon="mdi-account"
+                          height="28"
+                          color="purple"
+                          required
+                        ></v-text-field>
+                      </v-col>
+
+                      <v-col>
+                        <v-text-field
+                          v-model="lastName"
+                          label="Last name"
+                          :rules="nameRules"
+                          prepend-inner-icon="mdi-account"
+                          height="28"
+                          color="purple"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-text-field
+                      v-model="email"
+                      label="Email"
+                      :rules="emailRules"
+                      required
+                      prepend-inner-icon="mdi-email"
+                      height="28"
+                      color="purple"
+                    ></v-text-field>
+                    <v-text-field
+                      v-model="phoneNumber"
+                      label="Phone Number e.g +2567... or 07..."
+                      :rules="phoneNumberRules"
+                      required
+                      prepend-inner-icon="mdi-phone"
+                      height="28"
+                      color="purple"
+                    ></v-text-field>
+                    <v-text-field
+                      v-model="password"
+                      :rules="passwordRules(8)"
+                      label="Password"
+                      required
+                      :append-icon="
+                                                showPassword
+                                                    ? 'mdi-eye'
+                                                    : 'mdi-eye-off'
+                                            "
+                      :type="
+                                                showPassword
+                                                    ? 'text'
+                                                    : 'password'
+                                            "
+                      @click:append="
+                                                showPassword = !showPassword
+                                            "
+                      prepend-inner-icon="mdi-lock"
+                      height="28"
+                      color="purple"
+                    ></v-text-field>
+                    <v-text-field
+                      v-model="passwordConfirm"
+                      label="Confirm Password"
+                      :rules="passwordConfirmRules"
+                      required
+                      :append-icon="
+                                                showConfirm
+                                                    ? 'mdi-eye'
+                                                    : 'mdi-eye-off'
+                                            "
+                      :type="
+                                                showConfirm
+                                                    ? 'text'
+                                                    : 'password'
+                                            "
+                      @click:append="
+                                                showConfirm = !showConfirm
+                                            "
+                      prepend-inner-icon="mdi-lock"
+                      height="28"
+                      color="purple"
+                    ></v-text-field>
+                    <div class="mx-5">
+                      <v-btn
+                        type="submit"
+                        large
+                        width="400"
+                        rounded
+                        :loading="isLoading"
+                        depressed
+                        ripple
+                        class="yellow font-weight-bold"
+                        @click="register"
+                      >
+                        <v-icon>mdi-account-plus</v-icon>
+                        <span>&nbsp;Register</span>
+                      </v-btn>
+                    </div>
+                  </v-container>
+                </v-form>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-row>
+    </v-container>
+    <v-container class="d-block d-sm-none">
+      <v-form ref="registerForm" v-model="valid" class="text-center">
+        <v-row justify="start" align="center" no-gutters>
+          <v-col cols="1">
+            <v-btn to="/" icon>
+              <v-icon x-large color="black">mdi-arrow-left</v-icon>
+            </v-btn>
+          </v-col>
+          <v-col cols="11">
+            <span class="heading custom-font-family py-2">Register</span>
           </v-col>
         </v-row>
-      </v-card>
-    </v-row>
-  </v-container>
+
+        <v-alert
+          v-model="displaySignUpError"
+          type="error"
+          dismissible
+          class="mx-7 mt-2 error-alert"
+        >{{ signupError }}</v-alert>
+        <v-container class="container text-center">
+          <v-row no-gutters>
+            <v-col cols="12" xs="12">
+              <v-text-field
+                v-model="firstName"
+                label="First name"
+                :rules="nameRules"
+                prepend-inner-icon="mdi-account"
+                height="2rem"
+                class="styled-input sub-heading"
+                color="purple"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" xs="12">
+              <v-text-field
+                v-model="lastName"
+                label="Last name"
+                :rules="nameRules"
+                prepend-inner-icon="mdi-account"
+                class="mt-5 styled-input sub-heading"
+                height="2rem"
+                color="purple"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-text-field
+            v-model="email"
+            label="Email"
+            :rules="emailRules"
+            required
+            prepend-inner-icon="mdi-email"
+            class="mt-5 styled-input sub-heading"
+            height="2rem"
+            color="purple"
+          ></v-text-field>
+          <v-text-field
+            v-model="phoneNumber"
+            label="Phone Number e.g +2567... or 07..."
+            :rules="phoneNumberRules"
+            required
+            prepend-inner-icon="mdi-phone"
+            class="mt-5 styled-input sub-heading"
+            height="2rem"
+            color="purple"
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules(8)"
+            label="Password"
+            required
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+            class="mt-5 styled-input sub-heading"
+            prepend-inner-icon="mdi-lock"
+            height="2rem"
+            color="purple"
+          ></v-text-field>
+          <v-text-field
+            v-model="passwordConfirm"
+            label="Confirm Password"
+            :rules="passwordConfirmRules"
+            required
+            :append-icon="showConfirm ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showConfirm ? 'text' : 'password'"
+            class="mt-5 styled-input sub-heading"
+            @click:append="showConfirm = !showConfirm"
+            prepend-inner-icon="mdi-lock"
+            height="2rem"
+            color="purple"
+          ></v-text-field>
+          <div class="text-center my-3">
+            <v-btn
+              type="submit"
+              large
+              width="80%"
+              rounded
+              height="3.5rem"
+              :loading="isLoading"
+              depressed
+              ripple
+              class="yellow button-text font-weight-bold"
+              @click="register"
+            >
+              <v-icon x-large>mdi-account-plus</v-icon>
+              <span>&nbsp;Register</span>
+            </v-btn>
+          </div>
+        </v-container>
+      </v-form>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -179,7 +314,9 @@ export default {
                 this.$router.push(this.$route.params.continue);
               } else {
                 const user = this.user;
-                this.$router.push({ name: `${user.role}-dashboard` });
+                this.$router.push({
+                  name: `${user.role}-dashboard`
+                });
               }
             } else {
               this.displaySignUpError = true;
