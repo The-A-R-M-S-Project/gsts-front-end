05dcd801fefb38b01b324b8b423928636543303a
pip4pap <phillip.musumba.52@gmail.com>
2020-07-23 12:11:43 +0300

    Uses mobile navbar for mobile view on the dean's dashboard

--- src/views/DeanDashboard.vue
+++ src/views/DeanDashboard.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>
-    <DeanNav />
+  <div class="mx-auto overflow-hidden">
+    <DeanNav class="d-none d-sm-block" />
     <OverlayLoader />
+    <v-app-bar color="purple" class="mobile-drawer d-block d-sm-none" dark>
+      <v-toolbar-title>
+        <v-btn
+          text
+          class="normal-text px-0 text-capitalize white--text font-weight-bold"
+          href="/dean-dashboard"
+        >Dashboard</v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" class="d-flex d-sm-none" fixed bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item class="my-6" to="/under-construction">
+            <v-btn text class="title text-capitalize">
+              <v-icon large>mdi-account</v-icon>
+              <span>&nbsp;Profile</span>
+            </v-btn>
+          </v-list-item>
+          <v-list-item class="my-6" to="/students">
+            <v-btn text class="title text-capitalize">
+              <v-icon large>mdi-account-group</v-icon>
+              <span>&nbsp;Students</span>
+            </v-btn>
+          </v-list-item>
+          <v-list-item class="my-6" to="/examiners">
+            <v-btn text class="title text-capitalize">
+              <v-icon large>mdi-account-group</v-icon>
+              <span>&nbsp;Examiners</span>
+            </v-btn>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <template v-slot:append>
+        <v-list-item class="my-4" @click="logOut">
+          <v-btn text :loading="isLoading" class="title text-capitalize">
+            <v-icon large>mdi-power</v-icon>
+            <span>&nbsp;Sign out</span>
+          </v-btn>
+        </v-list-item>
+      </template>
+    </v-navigation-drawer>
     <v-container fluid class="pt-5">
       <v-row class="px-4">
         <v-row>
@@ -55,8 +98,27 @@ import OverlayLoader from "@/components/OverlayLoader.vue";
 import Calendar from "@/components/Calendar.vue";
 export default {
   name: "dean-dashboard",
+  data() {
+    return {
+      drawer: false,
+      loading: false,
+    };
+  },
+  computed: {
+    isLoading() {
+      return this.$store.getters.isLoading;
+    },
+  },
   created() {
     this.$store.dispatch("fetchDeanDashboardStats");
+  },
+  methods: {
+    logOut() {
+      this.closeOnContentClick = false;
+      this.$store.dispatch("logout").then(() => {
+        this.$router.push("/");
+      });
+    },
   },
   components: {
     DeanNav,
