75430135d890e842d1851502a2b156fac0ce0649
pip4pap <phillip.musumba.52@gmail.com>
2020-09-10 13:49:42 +0300

    Changes legend and names of axes on examiner record table

--- src/components/ExaminerRecord.vue
+++ src/components/ExaminerRecord.vue
@@ -13,31 +13,52 @@ export default {
         data: {
           datasets: [
             {
-              data: [{x: 10, y: 64},
-                     {x: 10, y: 65},
-                     {x: 15, y: 56},
-                     {x: 25, y: 65},
-                     {x: 35, y: 55},
-                     {x: 45, y: 57},
-                     {x: 45, y: 50},
-                     {x: 45, y: 54}],
-              backgroundColor: ["blue","orange","purple","red","yellow"],
+              data: [
+                { x: 10, y: 64 },
+                { x: 10, y: 65 },
+                { x: 15, y: 56 },
+                { x: 25, y: 65 },
+                { x: 35, y: 55 },
+                { x: 45, y: 57 },
+                { x: 45, y: 50 },
+                { x: 45, y: 54 },
+              ],
+              backgroundColor: ["blue", "orange", "purple", "red", "yellow"],
               fill: false,
-              lineTension: 0.1
-            }
+              lineTension: 0.1,
+            },
           ],
-          labels: ["2018", "2017", "2016", "2015", "2014"]
+          labels: ["2018", "2017", "2016", "2015", "2014"],
         },
         options: {
           legend: {
-            position: "bottom",
-            labels: {
-              fontColor: "black",
-              fontSize: 15
-            }
-          }
-        }
-      }
+            display: false,
+          },
+          scales: {
+            yAxes: [
+              {
+                scaleLabel: {
+                  display: true,
+                  labelString: "Number of students",
+                  fontColor: "black",
+                  fontSize: 14,
+                  padding: 6,
+                },
+              },
+            ],
+            xAxes: [
+              {
+                scaleLabel: {
+                  display: true,
+                  labelString: "Year",
+                  fontColor: "black",
+                  fontSize: 14,
+                },
+              },
+            ],
+          },
+        },
+      },
     };
   },
   methods: {
@@ -47,12 +68,12 @@ export default {
       const myChart = new Chart(ctx, {
         type: chartData.type,
         data: chartData.data,
-        options: chartData.options
+        options: chartData.options,
       });
-    }
+    },
   },
   mounted() {
     this.createChart("lineChart", this.chartData);
-  }
+  },
 };
 </script>
