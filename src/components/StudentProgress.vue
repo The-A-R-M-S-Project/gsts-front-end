8dd8e718d80acd5d4337f8f9842fe6fc5c94b415
pip4pap <phillip.musumba.52@gmail.com>
2020-09-24 12:08:45 +0300

    Changes timestamp to more standard format

--- src/components/StudentProgress.vue
+++ src/components/StudentProgress.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="pt-4">
     <div class="display-1 text-center font-weight-medium mb-4">Progress</div>
-    <v-row justify="center" :align="user.role==='student'?'center':'start'">
+    <v-row
+      justify="center"
+      :align="user.role === 'student' ? 'center' : 'start'"
+    >
       <v-col cols="12" md="4">
         <v-row>
           <v-col>
@@ -16,7 +19,7 @@
               >
                 {{ progressEvents[`${student.report.status}`].value }}%
                 <br />
-                {{progressEvents[`${student.report.status}`].message}}
+                {{ progressEvents[`${student.report.status}`].message }}
               </v-progress-circular>
             </div>
           </v-col>
@@ -27,32 +30,38 @@
       </v-col>
       <v-col cols="12" md="8" class="px-10">
         <v-stepper v-model="e6" vertical>
-          <v-stepper-step color="teal" :complete="e6 > 1" step="1">Report sent</v-stepper-step>
+          <v-stepper-step color="teal" :complete="e6 > 1" step="1"
+            >Report sent</v-stepper-step
+          >
           <template v-if="e6 > 1">
             <div class="v-stepper__content">
-              <div class="body-1 mb-1">Your report has been submitted to the right person</div>
-              <div
-                class="caption mt-3 font-weight-light"
-              >{{ formatDate(student.report.submittedAt) }}</div>
+              <div class="body-1 mb-1">Your report has been submitted.</div>
+              <div class="caption mt-3 font-weight-light">
+                {{ formatDate(student.report.submittedAt) }}
+              </div>
             </div>
           </template>
           <v-stepper-content step="1">
             <template v-if="e6 === 1">
               <div class="body-1 mb-1">Submit your report for grading</div>
-              <div v-show="user.role=== 'student'" class="body-2">
+              <div v-show="user.role === 'student'" class="body-2">
                 In case you haven't submitted your report, click
                 <a @click="submitReport">here</a>
               </div>
             </template>
           </v-stepper-content>
 
-          <v-stepper-step color="teal" :complete="e6 > 2" step="2">Report received by examiner</v-stepper-step>
+          <v-stepper-step color="teal" :complete="e6 > 2" step="2"
+            >Report received by examiner</v-stepper-step
+          >
           <template v-if="e6 > 2">
             <div class="v-stepper__content">
-              <div class="body-1">Your examiner has acknowledged receipt of your report</div>
-              <div
-                class="caption mt-3 font-weight-light"
-              >{{ formatDate(student.report.receivedAt) }}</div>
+              <div class="body-1">
+                Your examiner has acknowledged receipt of your report
+              </div>
+              <div class="caption mt-3 font-weight-light">
+                {{ formatDate(student.report.receivedAt) }}
+              </div>
             </div>
           </template>
           <v-stepper-content step="2">
@@ -65,21 +74,29 @@
             </template>
           </v-stepper-content>
 
-          <v-stepper-step color="teal" :complete="e6 > 3" step="3">Report returned by examiner</v-stepper-step>
+          <v-stepper-step color="teal" :complete="e6 > 3" step="3"
+            >Report returned by examiner</v-stepper-step
+          >
           <template v-if="e6 > 3">
             <div class="v-stepper__content">
-              <div class="body-1 mb-1">Your report has been marked and graded.</div>
+              <div class="body-1 mb-1">
+                Your report has been marked and graded.
+              </div>
               <div class="body-2">
                 You scored
                 <span class="subtitle-1">
                   <strong>
-                    <u>{{student.report.examinerScore}}% ({{ student.report.examinerGrade }})</u>
+                    <u
+                      >{{ student.report.examinerScore }}% ({{
+                        student.report.examinerGrade
+                      }})</u
+                    >
                   </strong>
                 </span>
               </div>
-              <div
-                class="caption mt-3 font-weight-light"
-              >{{ formatDate(student.report.examinerScoreDate) }}</div>
+              <div class="caption mt-3 font-weight-light">
+                {{ formatDate(student.report.examinerScoreDate) }}
+              </div>
             </div>
           </template>
           <v-stepper-content step="3">
@@ -87,48 +104,57 @@
               Your report is being marked and graded
               <LoadingDots />
             </div>
-            <div class="body-2">You'll be notified as soon as your score is available.</div>
+            <div class="body-2">
+              You'll be notified as soon as your score is available.
+            </div>
           </v-stepper-content>
 
-          <v-stepper-step color="teal" :complete="e6 > 4" step="4">Viva date set</v-stepper-step>
+          <v-stepper-step color="teal" :complete="e6 > 4" step="4"
+            >Viva date set</v-stepper-step
+          >
           <template v-if="e6 > 4">
             <div class="v-stepper__content">
               <div class="body-1 mb-1">
                 Your viva examination date has been set to
-                <span
-                  class="headline text-color blue--text"
-                >{{ formatDate(student.report.vivaDate) }}</span>
+                <span class="headline text-color blue--text">{{
+                  formatDate(student.report.vivaDate)
+                }}</span>
               </div>
               <div class="body-2">
                 <strong>Location:</strong> Cedat conference hall
               </div>
               <!-- This is supposed to be the date when the viva date was set by principal/dean -->
-              <div
-                class="caption mt-3 font-weight-light"
-              >{{ formatDate(student.report.examinerScoreDate) }}</div>
+              <div class="caption mt-3 font-weight-light">
+                {{ formatDate(student.report.examinerScoreDate) }}
+              </div>
             </div>
           </template>
           <v-stepper-content step="4">
-            <div class="body-1 mb-1">An appropriate date will be set for your viva examination.</div>
+            <div class="body-1 mb-1">
+              An appropriate date will be set for your viva examination.
+            </div>
           </v-stepper-content>
 
-          <v-stepper-step color="teal" :complete="e6 > 5" step="5">Viva examination</v-stepper-step>
+          <v-stepper-step color="teal" :complete="e6 > 5" step="5"
+            >Viva examination</v-stepper-step
+          >
           <template v-if="e6 > 5">
             <div class="v-stepper__content">
-              <div
-                class="body-1 mb-1"
-              >Congratulations! You've successfully completed your viva examination.</div>
+              <div class="body-1 mb-1">
+                Congratulations! You've successfully completed your viva
+                examination.
+              </div>
               <div class="body-2">
                 You scored
                 <span class="subtitle-1">
                   <strong>
-                    <u>{{student.report.vivaScore}}%</u>
+                    <u>{{ student.report.vivaScore }}%</u>
                   </strong>
                 </span>
               </div>
-              <div
-                class="caption mt-3 font-weight-light"
-              >{{ formatDate(student.report.vivaScoreDate) }}</div>
+              <div class="caption mt-3 font-weight-light">
+                {{ formatDate(student.report.vivaScoreDate) }}
+              </div>
             </div>
           </template>
         </v-stepper>
@@ -208,30 +234,15 @@ export default {
       this.$router.push("/student-dashboard/submit-report");
     },
     formatDate(timestamp) {
-      let diff = Math.abs(new Date() - new Date(timestamp));
-      let seconds = diff / 1000;
-      let minutes = Math.floor(seconds / 60);
-      let hours = Math.floor(minutes / 60);
-      let days = Math.floor(hours / 24);
-      if (seconds < 60) {
-        return "a few seconds ago";
-      } else if (minutes < 60) {
-        return `${minutes} minutes ago`;
-      } else if (hours < 24) {
-        return `${hours} hours ago`;
-      } else if (days < 7) {
-        return `${days} days ago`;
-      } else {
-        let monthDay = new Date(timestamp);
-        return `${monthDay.toLocaleTimeString(
-          {},
-          {
-            hour12: true,
-            hour: "numeric",
-            minute: "numeric",
-          }
-        )} ${new String(monthDay).substring(4, 15).replace(/ /g, "-")}`;
-      }
+      let monthDay = new Date(timestamp);
+      return `${monthDay.toLocaleTimeString(
+        {},
+        {
+          hour12: true,
+          hour: "numeric",
+          minute: "numeric",
+        }
+      )} ${new String(monthDay).substring(4, 15).replace(/ /g, "-")}`;
     },
   },
   components: {
