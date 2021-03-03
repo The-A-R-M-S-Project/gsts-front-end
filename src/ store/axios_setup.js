f584c927304c023eba168917fc37d620900922c0
pip4pap <phillip.musumba.52@gmail.com>
2021-03-03 16:39:21 +0300

    Points app to development server

--- src/ store/axios_setup.js
+++ src/ store/axios_setup.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_URL = "https://arms-graduate-student-tracker.herokuapp.com/api";
+// const API_URL = "https://arms-graduate-student-tracker.herokuapp.com/api";
+const API_URL = "http://localhost:8080/api"
 
 let settings = {
     baseURL: API_URL
