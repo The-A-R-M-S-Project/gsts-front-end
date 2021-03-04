d6330f12642dd4f8ce9062c80c855df7fea95a2d
pip4pap <phillip.musumba.52@gmail.com>
2021-03-04 12:51:40 +0300

    Allows for assigning of different types of examiners to a report

--- src/components/AssignExaminer.vue
+++ src/components/AssignExaminer.vue
@@ -2,13 +2,29 @@
   <div class="text-center">
     <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn v-bind="attrs" v-on="on" :loading="submitLoading" color="primary">Assign examiner</v-btn>
+        <v-btn v-bind="attrs" v-on="on" :loading="submitLoading" color="primary"
+          >Assign examiner</v-btn
+        >
       </template>
 
       <v-card>
-        <v-card-title class="text-center headline purple white--text">Assign an examiner</v-card-title>
+        <v-card-title class="text-center headline purple white--text"
+          >Assign an examiner</v-card-title
+        >
         <v-card-text class="py-3 px-6">
-          <p class="body-1">Assign an examiner to {{selectedStudent.student.name}}</p>
+          <p class="body-1">
+            Assign an examiner to {{ selectedStudent.student.lastName }}
+            {{ selectedStudent.student.firstName }}'s report
+          </p>
+          <p>
+            <v-icon color="primary">mdi-information-outline</v-icon>
+            If an examiner who has
+            <span class="text-decoration-underline">already been assigned</span>
+            to this report has
+            <span class="text-decoration-underline">not</span> yet accepted to
+            assess it, you may reassign them to this report under a different
+            <i>examiner type.</i>
+          </p>
           <v-form ref="selectExaminerForm">
             <v-autocomplete
               v-model="examiner"
@@ -21,6 +37,16 @@
               return-object
               color="purple"
             ></v-autocomplete>
+            <v-select
+              v-model="examinerType"
+              :items="examinerTypes"
+              item-text="name"
+              label="Select examiner type"
+              :rules="examinerRules"
+              color="purple"
+              class="mt-6"
+              @input="showType"
+            ></v-select>
           </v-form>
 
           <v-expand-transition>
@@ -28,7 +54,10 @@
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title>Name</v-list-item-title>
-                  <v-list-item-subtitle>{{ field.firstName }} {{ field.lastName }}</v-list-item-subtitle>
+                  <v-list-item-subtitle
+                    >{{ field.firstName }}
+                    {{ field.lastName }}</v-list-item-subtitle
+                  >
                 </v-list-item-content>
               </v-list-item>
               <v-list-item>
@@ -40,13 +69,17 @@
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title>Phone</v-list-item-title>
-                  <v-list-item-subtitle>{{ field.phoneNumber }}</v-list-item-subtitle>
+                  <v-list-item-subtitle>{{
+                    field.phoneNumber
+                  }}</v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title>Students</v-list-item-title>
-                  <v-list-item-subtitle>{{ field.students.length }}</v-list-item-subtitle>
+                  <v-list-item-title>Assigned Students</v-list-item-title>
+                  <v-list-item-subtitle>{{
+                    field.students.length
+                  }}</v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
             </v-list>
@@ -64,31 +97,31 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "AssignExaminer",
   data() {
     return {
       dialog: false,
       examiner: null,
-      examinerRules: [(examiner) => !!examiner || "Please select an examiner"],
+      examinerType: null,
+      examinerTypes: [
+        { name: "Internal", value: "internal" },
+        { name: "External", value: "external" },
+      ],
+      examinerRules: [(examiner) => !!examiner || "This field is required!"],
     };
   },
-  created() {
-    this.$store.dispatch("fetchExaminers");
+  async created() {
+    await this.$store.dispatch("fetchExaminers");
   },
   computed: {
-    selectedStudent() {
-      return this.$store.getters.selectedStudent;
-    },
+    ...mapGetters(["selectedStudent", "examiners", "submitLoading"]),
+
     field() {
       if (!this.examiner) return {};
       return this.examiner;
-    },
-    examiners() {
-      return this.$store.getters.examiners;
-    },
-    submitLoading() {
-      return this.$store.getters.submitLoading;
     },
   },
   methods: {
@@ -98,6 +131,7 @@ export default {
         this.$store
           .dispatch("assignExaminer", {
             examinerID: this.examiner._id,
+            examinerType: this.examinerType,
             studentReportID: this.selectedStudent._id,
             examinerName: assignedExaminer,
             studentName: this.selectedStudent.student.name,
