fe3182cabb9310c1be328960cbcede5e8bf35c4e
pip4pap <phillip.musumba.52@gmail.com>
2021-07-03 10:24:57 +0300

    Includes a note to warn users when retake details will stop displaying

--- src/components/RetakeDetails.vue
+++ src/components/RetakeDetails.vue
@@ -1,71 +1,79 @@
 <template>
-    <v-card class="mx-auto" :width="$vuetify.breakpoint.xs ? '90%' : '70%'">
-        <v-tabs
-            v-model="tab"
-            centered
-            grow
-            background-color="purple"
-            dark
-        >
-        <v-tabs-slider></v-tabs-slider>
+    <section>
+        <v-row align="center" justify="center">
+            <v-col :cols="$vuetify.breakpoint.xs ? '11' : '6'">
+                <v-icon color="primary">mdi-information-outline</v-icon>
+                These details will not appear after the student restarts their report submission process!
+            </v-col>
+        </v-row>
+        <v-card class="mx-auto" :width="$vuetify.breakpoint.xs ? '90%' : '70%'">
+            <v-tabs
+                v-model="tab"
+                centered
+                grow
+                background-color="purple"
+                dark
+            >
+            <v-tabs-slider></v-tabs-slider>
 
-        <v-tab href="#tab-1">
-            Grade
-        </v-tab>
+            <v-tab href="#tab-1">
+                Grade
+            </v-tab>
 
-        <v-tab href="#tab-2">
-            Assessments
-        </v-tab>
+            <v-tab href="#tab-2">
+                Assessments
+            </v-tab>
 
-        <v-tab href="#tab-3">
-            Report
-        </v-tab>
-        </v-tabs>
+            <v-tab href="#tab-3">
+                Report
+            </v-tab>
+            </v-tabs>
 
-        <v-tabs-items v-model="tab">
-            <v-tab-item :value="'tab-' + 1">
-                <v-card flat>
-                    <v-card-text>
-                        <p>
-                            <span class="body-1 font-weight-medium">Graded at:</span>
-                            <span>&nbsp;{{ formatDate(report.clearedAt) }}</span>
-                        </p>
-                        <p>
-                            <span class="body-1 font-weight-medium">Final score:</span>
-                            <span class="headline primary--text">&nbsp;{{ report.finalScore }}% (<strong>{{ report.grade }}</strong>)</span>
-                        </p>
-                    </v-card-text>
-                </v-card>
-            </v-tab-item>
-            <v-tab-item :value="'tab-' + 2">
-                <v-card flat>
-                    <v-card-text>
-                        <ExaminerAssessment />
-                    </v-card-text>
-                </v-card>
-            </v-tab-item>
-            <v-tab-item :value="'tab-' + 3">
-                <v-card flat>
-                    <v-card-text>
-                        <p>
-                            <span class="body-1 font-weight-medium">Title:</span>
-                            <span>&nbsp;{{ report.title }}</span>
-                        </p>
-                        <p>
-                            <span class="body-1 font-weight-medium">Abstract:</span>
-                            <span>&nbsp;{{ report.abstract }}</span>
-                        </p>
-                        <p>
-                            <span class="body-1 font-weight-medium">File:</span>
-                            <span>&nbsp;
-                                <a :href="report.reportURL" target="_blank">{{ report.reportURL }}</a>
-                            </span>
-                        </p>
-                    </v-card-text>
-                </v-card>
-            </v-tab-item>
-        </v-tabs-items>
-    </v-card>
+            <v-tabs-items v-model="tab">
+                <v-tab-item :value="'tab-' + 1">
+                    <v-card flat>
+                        <v-card-text>
+                            <p>
+                                <span class="body-1 font-weight-medium">Graded at:</span>
+                                <span>&nbsp;{{ formatDate(report.clearedAt) }}</span>
+                            </p>
+                            <p>
+                                <span class="body-1 font-weight-medium">Final score:</span>
+                                <span class="headline primary--text">&nbsp;{{ report.finalScore }}% (<strong>{{ report.grade }}</strong>)</span>
+                            </p>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item :value="'tab-' + 2">
+                    <v-card flat>
+                        <v-card-text>
+                            <ExaminerAssessment />
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+                <v-tab-item :value="'tab-' + 3">
+                    <v-card flat>
+                        <v-card-text>
+                            <p>
+                                <span class="body-1 font-weight-medium">Title:</span>
+                                <span>&nbsp;{{ report.title }}</span>
+                            </p>
+                            <p>
+                                <span class="body-1 font-weight-medium">Abstract:</span>
+                                <span>&nbsp;{{ report.abstract }}</span>
+                            </p>
+                            <p>
+                                <span class="body-1 font-weight-medium">File:</span>
+                                <span>&nbsp;
+                                    <a :href="report.reportURL" target="_blank">{{ report.reportURL }}</a>
+                                </span>
+                            </p>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs-items>
+        </v-card>
+    </section>
 </template>
 
 <script>
